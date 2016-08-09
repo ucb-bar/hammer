db0217cf1609a7094eefb7fcd59e8d63e74aeda0
Palmer Dabbelt <palmer@dabbelt.com>
2016-08-09 11:20:13 -0700

    Don't have the crossbar list any tests
    I thought I did this earlier, but maybe I dropped it or something.  It
    was certainly half-way done!

--- src/addons/core-generator/crossbar/rules.mk
+++ src/addons/core-generator/crossbar/rules.mk
@@ -1,7 +1,8 @@
 # Copyright 2016 Palmer Dabbelt <palmer@dabbelt.com>
 
 # The crossbar can't actually run any tests itself, so instead I'm 
-$(OBJ_CORE_DIR)/empty-tests.mk:
+$(OBJ_CORE_DIR)/core-crossbar/empty-tests.mk:
+	@mkdir -p $(dir $@)
 	touch $@
 
 include src/addons/core-generator/rocket-chip/rules.mk
--- src/addons/core-generator/crossbar/vars.mk
+++ src/addons/core-generator/crossbar/vars.mk
@@ -5,5 +5,6 @@ CORE_ADDON_DIR = $(CORE_GENERATOR_ADDON)/src
 
 OBJ_CORE_RTL_TB_CPP = $(CORE_GENERATOR_ADDON)/csrc/$(CORE_TOP).$(CORE_CONFIG).tb.cpp
 OBJ_CORE_RTL_C =
+OBJ_CORE_TESTS_MK = $(OBJ_CORE_DIR)/core-crossbar/empty-tests.mk
 
 include src/addons/core-generator/rocket-chip/vars.mk
