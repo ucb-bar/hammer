fe89365c8415a4fc8e7bc2f52742494e2e58f9bb
Palmer Dabbelt <palmer@dabbelt.com>
2016-08-25 09:59:27 -0700

    Update bar-testchip to use the new Rocket Chip infastructure
    This is actually a bit of a pain: this used to simulate correctly but
    wouldn't actually produce a HTIF-wrapped chip.  I guess we'd notice the
    problem later at packaging time, but it would be better if I could test
    this automatically.

--- src/addons/soc-generator/bar-testchip/rules.mk
+++ src/addons/soc-generator/bar-testchip/rules.mk
@@ -7,7 +7,7 @@ OBJ_SOC_RTL_CHISEL_V = $(OBJ_SOC_DIR)/chisel/generated-src/$(SOC_TOP).v
 # Generates some wrappers that make $(SOC_TOP_FOR_SYNTHESIS) a suitable ASIC
 # top-level design and $(SOC_TOP) contain a wrapper 
 $(OBJ_SOC_RTL_V): $(OBJ_SOC_RTL_CHISEL_V) $(OBJ_CORE_RTL_V)
-	cat $^ > $@
+	cat $^ | sed 's@Top dut@TestTop dut@g' > $@
 
 $(OBJ_SOC_RTL_CHISEL_V): $(OBJ_SOC_RTL_CHISEL) $(CMD_SBT)
 	$(SOC_GENERATOR_ADDON)/tools/run-chisel --top $(SOC_TOP) --output $(abspath $(dir $@)) $(abspath $^)
@@ -19,16 +19,3 @@ $(OBJ_SOC_RTL_CHISEL): $(OBJ_SOC_RTL_DECOUPLED_JSON) $(SOC_GENERATOR_ADDON)/tool
 $(OBJ_SOC_RTL_DECOUPLED_JSON): $(CMD_PCAD_INFER_DECOUPLED) $(OBJ_CORE_RTL_V)
 	mkdir -p $(dir $@)
 	$(CMD_PCAD_INFER_DECOUPLED) -o $@ --top $(CORE_TOP) -i $(filter %.v,$^)
-
-# The test harnesses need to change because the name of the top-level module
-# has changed.  This sed is dangerous, but I don't want to talk to upstream so
-# I'm just going to live with it...
-$(OBJ_SOC_DIR)/csrc/%.cc: $(OBJ_CORE_RTL_C) $(patsubst %,$(OBJ_SOC_DIR)/csrc/%,$(notdir $(wildcard $(patsubst %,%/*.h,$(sort $(dir $(OBJ_CORE_RTL_C)))))))
-	mkdir -p $(dir $@)
-	cat $(filter %/$(notdir $@),$^) | sed 's/$(CORE_TOP)/$(SOC_TOP)/g' > $@
-
-# So this is a huge hack: essentially what happens is that loads of 
-.SECONDARY: $(patsubst %,$(OBJ_SOC_DIR)/csrc/%,$(notdir $(wildcard $(patsubst %,%/*.h,$(sort $(dir $(OBJ_CORE_RTL_C)))))))
-$(OBJ_SOC_DIR)/csrc/%.h: $(wildcard $(patsubst %,%/*.h,$(sort $(dir $(OBJ_CORE_RTL_C)))))
-	mkdir -p $(dir $@)
-	cp -f $(filter %/$(notdir $@),$^) $@
