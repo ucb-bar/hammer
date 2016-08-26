a6dea4a911755dececdb6ac1d56a5d5b35138a62
Palmer Dabbelt <palmer@dabbelt.com>
2016-08-26 15:57:04 -0700

    Don't call the VCS rules "verilator-*"

--- src/addons/simulator/vcs/core-vars.mk
+++ src/addons/simulator/vcs/core-vars.mk
@@ -3,8 +3,8 @@
 include $(CORE_SIMULATOR_ADDON)/_vars.mk
 
 ifeq ($(filter $(MAKECMDGOALS),clean distclean),)
--include $(OBJ_CORE_DIR)/verilator-vars.mk
-$(OBJ_CORE_DIR)/verilator-vars.mk: $(CORE_SIMULATOR_ADDON)/tools/generate-vars
+-include $(OBJ_CORE_DIR)/vcs-vars.mk
+$(OBJ_CORE_DIR)/vcs-vars.mk: $(CORE_SIMULATOR_ADDON)/tools/generate-vars
 	mkdir -p $(dir $@)
 	$< --output $@ --upper CORE --lower core
 endif
--- src/addons/simulator/vcs/soc-vars.mk
+++ src/addons/simulator/vcs/soc-vars.mk
@@ -3,8 +3,8 @@
 include $(SOC_SIMULATOR_ADDON)/_vars.mk
 
 ifeq ($(filter $(MAKECMDGOALS),clean distclean),)
--include $(OBJ_SOC_DIR)/verilator-vars.mk
-$(OBJ_SOC_DIR)/verilator-vars.mk: $(SOC_SIMULATOR_ADDON)/tools/generate-vars
+-include $(OBJ_SOC_DIR)/vcs-vars.mk
+$(OBJ_SOC_DIR)/vcs-vars.mk: $(SOC_SIMULATOR_ADDON)/tools/generate-vars
 	mkdir -p $(dir $@)
 	$< --output $@ --upper SOC --lower soc
 endif
