dd393d5fd17887cadbd47f4f995d74914cf4403a
Palmer Dabbelt <palmer@dabbelt.com>
2016-08-26 16:24:39 -0700

    Check for a Synopsys license file before running VCS
    This gives me a slightly better (and earlier) error message.

--- src/addons/simulator/vcs/_vars.mk
+++ src/addons/simulator/vcs/_vars.mk
@@ -8,6 +8,10 @@ ifeq ($(VCS_VERSION),)
 $(error Specify a VCS version as VCS_VERSION)
 endif
 
+ifeq ($(SNPSLMD_LICENSE_FILE),)
+$(error SNPSLMD_LICENSE_FILE needs to be exported for VCS to run)
+endif
+
 VCS_HOME = $(SYNOPSYS_HOME)/vcs/$(VCS_VERSION)
 VCS_BIN = $(VCS_HOME)/bin/vcs
 
