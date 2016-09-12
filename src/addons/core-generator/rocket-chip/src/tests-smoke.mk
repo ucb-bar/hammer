170fb82b85264b16eaa3116838b3964d7c7eafc4
Palmer Dabbelt <palmer@dabbelt.com>
2016-09-12 16:21:20 -0700

    Call the simulator outputs "trace-out", not "vpd-out"
    Verilator doesn't generate VPDs, so calling this vpdout is kind of odd.

--- src/addons/core-generator/rocket-chip/src/tests-smoke.mk
+++ src/addons/core-generator/rocket-chip/src/tests-smoke.mk
@@ -1,7 +1,7 @@
 check-core: $(CHECK_CORE_DIR)/rv64ui-p-simple.out
-check-core-vpd: $(CHECK_CORE_DIR)/rv64ui-p-simple.vpdout
+trace-core: $(CHECK_CORE_DIR)/rv64ui-p-simple.trace-out
 $(CHECK_CORE_DIR)/rv64ui-p-simple.out: $(OBJ_CORE_DIR)/riscv-tests/rv64ui-p-simple
 
 check-soc: $(CHECK_SOC_DIR)/rv64ui-p-simple.out
-check-soc-vpd: $(CHECK_SOC_DIR)/rv64ui-p-simple.vpdout
+trace-soc: $(CHECK_SOC_DIR)/rv64ui-p-simple.trace-out
 $(CHECK_SOC_DIR)/rv64ui-p-simple.out: $(OBJ_CORE_DIR)/riscv-tests/rv64ui-p-simple
