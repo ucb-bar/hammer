a875605029e2d8731fb30a3f263d85bba447100a
Palmer Dabbelt <palmer@dabbelt.com>
2016-09-22 18:22:27 -0700

    Use a stamp file for DC's output

--- src/addons/synthesis/dc/rules.mk
+++ src/addons/synthesis/dc/rules.mk
@@ -1,6 +1,15 @@
 # Copyright 2016 Palmer Dabbelt <palmer@dabbelt.com>
 
-$(OBJ_SYN_MAPPED_V): \
+# This rule is capable of producing all the various output files from DC after
+# DC has successfully run.
+$(OBJ_SYN_MAPPED_V): $(OBJ_SYN_DIR)/synopsys-dc.stamp
+	@mkdir -p $(dir $@)
+	cp --reflink=auto $(OBJ_SYN_DIR)/synopsys-dc-workdir/results/$(notdir $@) $@
+
+# DC produces a lot of outputs, runs for a long time, and is kind of flaky
+# about producing arbitrary outputs.  Rather than relying on DC's output files
+# for dependency resolution we instead.
+$(OBJ_SYN_DIR)/synopsys-dc.stamp: \
 		$(abspath $(SYNTHESIS_TOOL_ADDON)/run-synthesis) \
 		$(OBJ_SOC_RTL_V) \
 		$(patsubst %,%/lib,$(TECHNOLOGY_MILKYWAY_DIRS)) \
@@ -10,4 +19,5 @@ $(OBJ_SYN_MAPPED_V): \
 		$(PLSI_CACHE_DIR)/synopsys/rm/DC-RM_$(DC_VERSION).tar \
 		$(DC_BIN)
 	@mkdir -p $(dir $@)
-	$(SCHEDULER_CMD) --max-threads=1 -- $(abspath $<) --top $(SYN_TOP) -o $(abspath $@) $(abspath $^)
+	$(SCHEDULER_CMD) --max-threads=1 -- $(abspath $<) --top $(SYN_TOP) --output_dir $(abspath $(dir $@))/synopsys-dc-workdir $(abspath $^)
+	date > $@
