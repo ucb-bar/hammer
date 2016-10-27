5582bca04afb9f14861728ee088997cf609f4ad2
Colin Schmidt <colins@eecs.berkeley.edu>
2016-10-27 14:49:32 -0700

    start using hwacha-chip as a hwacha plsi addon

---
+++ src/addons/core-generator/hwacha/vars.mk
@@ -0,0 +1,6 @@
+# Copyright 2016 Palmer Dabbelt <palmer@dabbelt.com>
+
+CORE_DIR = $(CORE_GENERATOR_ADDON)/hwacha-chip
+RC_CORE_ADDONS = hwacha
+
+include src/addons/core-generator/rocket-chip/vars.mk
