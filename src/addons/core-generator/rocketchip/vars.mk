a25714f77b04d67e2e4800dc72da2c7d72527ac7
Palmer Dabbelt <palmer@dabbelt.com>
2017-01-27 11:57:43 -0800

    Add the JTAG DTM to Rocket Chip

--- src/addons/core-generator/rocketchip/vars.mk
+++ src/addons/core-generator/rocketchip/vars.mk
@@ -15,6 +15,7 @@ CORE_SIM_TOP ?= $(RC_CORE_SIM_TOP)
 # This contains the whole Rocket Chip along with all the test harness stuff.
 RC_OBJ_CORE_RTL_V = \
 	$(OBJ_CORE_DIR)/plsi-generated/$(CORE_TOP).$(CORE_CONFIG).v \
+	$(CORE_DIR)/vsrc/DebugTransportModuleJtag.v \
 	$(CORE_DIR)/vsrc/AsyncResetReg.v
 OBJ_CORE_RTL_V ?= $(RC_OBJ_CORE_RTL_V)
 
@@ -47,7 +48,9 @@ RC_OBJ_CORE_SIM_FILES = \
 	$(CORE_DIR)/vsrc/TestDriver.v \
 	$(OBJ_CORE_DIR)/plsi-generated/$(CORE_SIM_TOP).$(CORE_CONFIG).v \
 	src/addons/core-generator/rocketchip/src/clock.vh \
-	$(OBJ_CORE_DIR)/plsi-generated/model.vh
+	$(OBJ_CORE_DIR)/plsi-generated/model.vh \
+	$(CORE_DIR)/vsrc/jtag_vpi.v \
+	$(CORE_DIR)/vsrc/jtag_vpi.tab
 OBJ_CORE_SIM_FILES = $(RC_OBJ_CORE_SIM_FILES)
 
 # These files provide Verilog implementations of the macros, and might be
