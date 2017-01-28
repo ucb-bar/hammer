f3f580d81752fab46ae384841ed642e0ee1a4c42
Donggyu Kim <dgkim@eecs.berkeley.edu>
2017-01-28 15:25:26 -0800

    optionally run verify

--- src/addons/formal/formality/syn-rules.mk
+++ src/addons/formal/formality/syn-rules.mk
@@ -9,4 +9,4 @@ $(CHECK_SYN_DIR)/formality-$(MAP_TOP)-$(SYN_TOP).out: \
 		$(TECHNOLOGY_VERILOG_FILES) \
 		$(TECHNOLOGY_CCS_LIBRARY_FILES)
 	mkdir -p $(dir $@)
-	$(SCHEDULER_CMD) -- $(CMD_PTEST) --test $(abspath $<) --out $(abspath $@) --args $(abspath $(FORMALITY_BIN)) --dc-dir $(abspath $(OBJ_SYN_DIR)/synopsys-dc-workdir)
+	$(SCHEDULER_CMD) -- $(CMD_PTEST) --test $(abspath $<) --out $(abspath $@) --args $(abspath $(FORMALITY_BIN)) --dc-dir $(abspath $(OBJ_SYN_DIR)/synopsys-dc-workdir) $(if $(FORMALITY_NO_VERIFY),--no-verify)
