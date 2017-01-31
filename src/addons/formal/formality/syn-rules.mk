93e30abc0447af7d641b8bbd0e84d8cac2ac0da4
Palmer Dabbelt <palmer@dabbelt.com>
2017-01-31 10:37:22 -0800

    Put the Formality runs in the signoff directory

--- src/addons/formal/formality/syn-rules.mk
+++ src/addons/formal/formality/syn-rules.mk
@@ -1,12 +1,12 @@
 # See LICENSE for details
 
-check-syn: $(CHECK_SYN_DIR)/formality-$(MAP_TOP)-$(SYN_TOP).out
+check-syn: $(SIGNOFF_SYN_FORMAL_DIR)/formality-$(MAP_TOP)-$(SYN_TOP).out
 
-$(CHECK_SYN_DIR)/formality-$(MAP_TOP)-$(SYN_TOP).out: \
+$(SIGNOFF_SYN_FORMAL_DIR)/formality-$(MAP_TOP)-$(SYN_TOP).out: \
 		$(SYN_FORMAL_ADDON)/run-formality \
 		$(OBJ_MAP_RTL_V) \
 		$(OBJ_SYN_MAPPED_V) \
 		$(TECHNOLOGY_VERILOG_FILES) \
 		$(TECHNOLOGY_CCS_LIBRARY_FILES)
-	mkdir -p $(dir $@)
+	@mkdir -p $(dir $@)
 	$(SCHEDULER_CMD) -- $(CMD_PTEST) --test $(abspath $<) --out $(abspath $@) --args $(abspath $(FORMALITY_BIN)) --dc-dir $(abspath $(OBJ_SYN_DIR)/synopsys-dc-workdir) $(if $(FORMALITY_NO_VERIFY),--no-verify)
