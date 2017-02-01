c12aeaa42ef8c9c01a3f13f98cc46a71dee448b5
Palmer Dabbelt <palmer@dabbelt.com>
2017-02-01 09:42:30 -0800

    Remove the BOOM core generator
    I'm moving BOOM and Hwacha into my masters' thesis repo so I don't have
    to track all sorts of junk upstream.

--- src/addons/core-generator/boom/rules.mk
+++
@@ -1,3 +0,0 @@
-# Copyright 2016 Palmer Dabbelt <palmer@dabbelt.com>
-
-include src/addons/core-generator/rocket-chip/rules.mk
--- src/addons/core-generator/boom/vars.mk
+++
@@ -1,6 +0,0 @@
-# Copyright 2016 Palmer Dabbelt <palmer@dabbelt.com>
-
-RC_CORE_ADDON_DIRS = $(CORE_GENERATOR_ADDON)/src/boom
-RC_CORE_CFG_PROJECT = boom
-
-include src/addons/core-generator/rocket-chip/vars.mk
