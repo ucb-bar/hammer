20f371c2c4e8607aaa4365a39f3119f41d7cd7e9
Donggyu Kim <dgkim@eecs.berkeley.edu>
2017-02-07 11:27:56 -0800

    pass DC's compile/compile_ultra arguments through DC_COMPILE_ARGS

--- src/addons/synthesis/dc/rules.mk
+++ src/addons/synthesis/dc/rules.mk
@@ -29,7 +29,7 @@ $(OBJ_SYN_DIR)/synopsys-dc.stamp: \
 		$(TECHNOLOGY_JSON) \
 		$(DC_BIN)
 	@mkdir -p $(dir $@)
-	$(SCHEDULER_CMD) --max-threads=16 -- $(abspath $<) --top $(SYN_TOP) --output_dir $(abspath $(dir $@))/synopsys-dc-workdir $(abspath $^)
+	$(SCHEDULER_CMD) --max-threads=16 -- $(abspath $<) --top $(SYN_TOP) --output_dir $(abspath $(dir $@))/synopsys-dc-workdir $(abspath $^) $(foreach arg, $(DC_COMPILE_ARGS), --compile_arg $(arg))
 	date > $@
 
 # FIXME: None of these are DC-specific, so they should probably live somewhere else.
