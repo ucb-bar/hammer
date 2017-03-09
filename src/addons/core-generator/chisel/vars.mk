6270d0eb1fec6ae95ffbc42a43204a31a010c595
Edward Wang <edward.c.wang@compdigitec.com>
2017-03-08 18:12:33 -0800

    Show an error instead of failing silently

--- src/addons/core-generator/chisel/vars.mk
+++ src/addons/core-generator/chisel/vars.mk
@@ -27,6 +27,15 @@ CORE_CONFIG_VIVADO_PINMAP ?= $(CORE_CONFIG_PROJ_DIR)/pinmap.json
 OBJ_CORE_SIM_FILES = $(CORE_CONFIG_PROJ_DIR)/src/$(CORE_TOP)Harness.v
 OBJ_CORE_SIM_MACRO_FILES = $(CORE_CONFIG_PROJ_DIR)/src/$(CORE_TOP).simulation_macros.v
 OBJ_CORE_MACROS = $(CORE_CONFIG_PROJ_DIR)/src/$(CORE_TOP).macros.json
+ifeq (,$(wildcard $(OBJ_CORE_SIM_FILES)))
+    $(error Verilog test harness does not exist! If it is not required, run "touch $(OBJ_CORE_SIM_FILES)" to create a blank file)
+endif
+ifeq (,$(wildcard $(OBJ_CORE_SIM_MACRO_FILES)))
+    $(error Verilog sim macro file does not exist! If it is not required, run "touch $(OBJ_CORE_SIM_MACRO_FILES)" to create a blank file)
+endif
+ifeq (,$(wildcard $(OBJ_CORE_MACROS)))
+    $(error Verilog sim macros file does not exist! If it is not required, run "echo "[]" > $(OBJ_CORE_MACROS)" to create a blank file)
+endif
 
 OBJ_CORE_RTL_FIR = $(OBJ_CORE_DIR)/generated/$(CORE_TOP).fir
 OBJ_CORE_RTL_V = $(OBJ_CORE_DIR)/generated/$(CORE_TOP).v
