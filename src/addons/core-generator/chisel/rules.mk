67bdc4b7d02ab7de7c0c358fe8255c9172fec6bc
Edward Wang <edward.c.wang@compdigitec.com>
2017-03-15 14:46:22 -0700

    Automatically generate macros for chisel core-generator

--- src/addons/core-generator/chisel/rules.mk
+++ src/addons/core-generator/chisel/rules.mk
@@ -10,7 +10,14 @@ $(OBJ_CORE_RTL_FIR): \
 	$(SCHEDULER_CMD) --max-threads=1 -- $< --sbt $(abspath $(CMD_SBT)) --syn-top $(CORE_TOP) --sim-top $(CORE_SIM_TOP) --top-package $(CORE_CONFIG_TOP_PACKAGE) --srcdir $(abspath $(CORE_CONFIG_PROJ_DIR)) --outdir $(abspath $(dir $@))
 
 # Generates a Verilog file from the FIRRTL representation.
-$(OBJ_CORE_RTL_V): \
+$(OBJ_CORE_RTL_V) $(OBJ_CORE_CHISEL_MEM_CONFIG): \
 		$(CMD_FIRRTL_GENERATE_TOP) \
 		$(OBJ_CORE_RTL_FIR)
-	$(SCHEDULER_CMD) --max-threads=1 -- $< -i $(abspath $(filter %.fir,$^)) -o $(abspath $@) --syn-top $(CORE_TOP) --harness-top $(CORE_SIM_TOP)
+	$(SCHEDULER_CMD) --max-threads=1 -- $< -i $(abspath $(filter %.fir,$^)) -o $(abspath $@) --syn-top $(CORE_TOP) --harness-top $(CORE_SIM_TOP) --seq-mem-flags "-o:$(abspath $(OBJ_CORE_CHISEL_MEM_CONFIG))"
+
+# Generate the PLSI macros file using the generated memories config from FIRRTL.
+$(OBJ_CORE_MACROS): \
+		$(OBJ_CORE_CHISEL_MEM_CONFIG) \
+		$(CORE_GENERATOR_ADDON)/tools/generate-macros \
+		$(CMD_PYTHON3)
+	PATH="$(abspath $(dir $(CMD_PYTHON3))):$(PATH)" $(CORE_GENERATOR_ADDON)/tools/generate-macros --macros $(abspath $(OBJ_CORE_CHISEL_MEM_CONFIG)) --output $(abspath $@)
--- src/addons/core-generator/chisel/vars.mk
+++ src/addons/core-generator/chisel/vars.mk
@@ -26,19 +26,19 @@ CORE_CONFIG_VIVADO_PINMAP ?= $(CORE_CONFIG_PROJ_DIR)/pinmap.json
 # Maybe also add checks for existence here for debugging.
 OBJ_CORE_SIM_FILES = $(CORE_CONFIG_PROJ_DIR)/src/$(CORE_TOP)Harness.v
 OBJ_CORE_SIM_MACRO_FILES = $(CORE_CONFIG_PROJ_DIR)/src/$(CORE_TOP).simulation_macros.v
-OBJ_CORE_MACROS = $(CORE_CONFIG_PROJ_DIR)/src/$(CORE_TOP).macros.json
 ifeq (,$(wildcard $(OBJ_CORE_SIM_FILES)))
     $(error Verilog test harness does not exist! If it is not required, run "touch $(OBJ_CORE_SIM_FILES)" to create a blank file)
 endif
 ifeq (,$(wildcard $(OBJ_CORE_SIM_MACRO_FILES)))
     $(error Verilog sim macro file does not exist! If it is not required, run "touch $(OBJ_CORE_SIM_MACRO_FILES)" to create a blank file)
 endif
-ifeq (,$(wildcard $(OBJ_CORE_MACROS)))
-    $(error Verilog sim macros file does not exist! If it is not required, run "echo "[]" > $(OBJ_CORE_MACROS)" to create a blank file)
-endif
 
+# Generated targets.
+
+OBJ_CORE_MACROS = $(OBJ_CORE_DIR)/generated/$(CORE_TOP).macros.json
 OBJ_CORE_RTL_FIR = $(OBJ_CORE_DIR)/generated/$(CORE_TOP).fir
 OBJ_CORE_RTL_V = $(OBJ_CORE_DIR)/generated/$(CORE_TOP).v
+OBJ_CORE_CHISEL_MEM_CONFIG = $(OBJ_CORE_DIR)/generated/mem.config
 
 # If you don't define something here then there won't be any test cases that
 # can actually run.
