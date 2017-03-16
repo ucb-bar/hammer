fc94e7a6ece711c8efb6a76b9d0b997c80915025
Edward Wang <edward.c.wang@compdigitec.com>
2017-03-15 18:18:49 -0700

    Depend on the template as well

--- src/addons/soc-generator/vivado/rules.mk
+++ src/addons/soc-generator/vivado/rules.mk
@@ -5,6 +5,7 @@
 $(OBJ_SOC_SYSTEM_TOP_V): \
 	$(abspath $(SOC_GENERATOR_ADDON)/generate-system-v) \
 	$(OBJ_SOC_PIN_MAP_DATABASE) \
+	$(SOC_GENERATOR_ADDON)/src/system.template.v \
 	$(CORE_CONFIG_VIVADO_PINMAP)
 	@mkdir -p $(dir $@)
 	$(SCHEDULER_CMD) --max-threads=1 -- $(abspath $<) -o $(abspath $@) --top $(SYN_TOP) --pinmap $(abspath $(CORE_CONFIG_VIVADO_PINMAP)) --database $(abspath $(OBJ_SOC_PIN_MAP_DATABASE)) --template $(abspath $(SOC_GENERATOR_ADDON)/src/system.template.v)
