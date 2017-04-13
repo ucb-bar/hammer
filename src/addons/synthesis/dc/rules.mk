5b4c2f5016487156600ffe8f5feac584467ac052
Edward Wang <edward.c.wang@compdigitec.com>
2017-04-13 15:53:29 -0700

    dc: run-synthesis uses python3

--- src/addons/synthesis/dc/rules.mk
+++ src/addons/synthesis/dc/rules.mk
@@ -31,7 +31,7 @@ $(OBJ_SYN_DIR)/synopsys-dc.stamp: \
 		$(TECHNOLOGY_JSON) \
 		$(DC_BIN)
 	@mkdir -p $(dir $@)
-	$(SCHEDULER_CMD) --max-threads=16 -- $(abspath $<) --top $(SYN_TOP) --output_dir $(abspath $(dir $@))/synopsys-dc-workdir $(abspath $^) $(foreach arg, $(DC_COMPILE_ARGS), --compile_arg $(arg))
+	PATH="$(abspath $(dir $(CMD_PYTHON3))):$(PATH)" $(SCHEDULER_CMD) --max-threads=16 -- $(abspath $<) --top $(SYN_TOP) --output_dir $(abspath $(dir $@))/synopsys-dc-workdir $(abspath $^) $(foreach arg, $(DC_COMPILE_ARGS), --compile_arg $(arg))
 	date > $@
 
 # FIXME: None of these are DC-specific, so they should probably live somewhere else.
