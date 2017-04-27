0c1322366647b83ed75f2ef8cac8de3dada7f8ae
Donggyu Kim <dgkim@eecs.berkeley.edu>
2017-04-26 19:55:21 -0700

    fix primetime scripts

--- src/addons/signoff-power/primetime/syn-rules.mk
+++ src/addons/signoff-power/primetime/syn-rules.mk
@@ -12,9 +12,9 @@ $(OBJ_SYN_DIR)/pt-power/%/stamp: \
 		$(VCD2SAIF_BIN) $(VPD2VCD_BIN) \
 		$(TECHNOLOGY_CCS_LIBRARY_FILES) \
 		$(PLSI_CACHE_DIR)/synopsys/rm/PT-RM_$(PRIMETIME_POWER_VERSION).tar \
-		$(SYN_CONFIG_FILE)
+		$(PLSI_CAD_CONFIG_FILE)
 	mkdir -p $(dir $@)
-	$(SCHEDULER_CMD) --max-threads=1 -- $(abspath $<) --output $(abspath $@) --top $(SYN_TOP) $(abspath $^)
+	$(SCHEDULER_CMD) --max-threads=1 -- $(abspath $<) --output $(abspath $@) --top $(SYN_TOP) --testbench $(TESTBENCH) $(abspath $^)
 
 $(PLSI_CACHE_DIR)/synopsys/rm/PT-RM_$(PRIMETIME_POWER_VERSION).tar: $(SYNOPSYS_RM_DIR)/PT-RM_$(PRIMETIME_POWER_VERSION).tar
 	cp --reflink=auto $< $@
--- src/addons/signoff-power/primetime/syn-vars.mk
+++ src/addons/signoff-power/primetime/syn-vars.mk
@@ -1,18 +1,18 @@
 # See LICENSE for details
 
 ifeq ($(SYNOPSYS_HOME),)
-$(error Running Formality requires SYNOPSYS_HOME to be set)
+$(error Running Primetime requires SYNOPSYS_HOME to be set)
 endif
 
 ifeq ($(PRIMETIME_POWER_VERSION),)
-$(error Specify a Formality version as PRIMETIME_POWER_VERSION)
+$(error Specify a Primetime version as PRIMETIME_POWER_VERSION)
 endif
 
 PRIMETIME_POWER_HOME = $(SYNOPSYS_HOME)/pts/$(PRIMETIME_POWER_VERSION)
 PRIMETIME_POWER_BIN = $(PRIMETIME_POWER_HOME)/bin/pt_shell
 
 ifeq ($(wildcard $(PRIMETIME_POWER_BIN)),)
-$(error Cannot find formality at $(PRIMETIME_POWER_BIN))
+$(error Cannot find Primetime at $(PRIMETIME_POWER_BIN))
 endif
 
 VCD2SAIF_BIN = $(SYNOPSYS_HOME)/syn/$(DC_VERSION)/bin/vcd2saif
