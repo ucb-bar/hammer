6cdb267b142abc28555c107d29f768d926b5bba6
Edward Wang <edward.c.wang@compdigitec.com>
2017-06-08 02:08:18 -0700

    primetime: have a common vars

---
+++ src/addons/signoff-power/primetime/vars.mk
@@ -0,0 +1,26 @@
+# See LICENSE for details
+
+ifeq ($(SYNOPSYS_HOME),)
+$(error Running Primetime requires SYNOPSYS_HOME to be set)
+endif
+
+ifeq ($(PRIMETIME_POWER_VERSION),)
+$(error Specify a Primetime version as PRIMETIME_POWER_VERSION)
+endif
+
+PRIMETIME_POWER_HOME = $(SYNOPSYS_HOME)/pts/$(PRIMETIME_POWER_VERSION)
+PRIMETIME_POWER_BIN = $(PRIMETIME_POWER_HOME)/bin/pt_shell
+
+ifeq ($(wildcard $(PRIMETIME_POWER_BIN)),)
+$(error Cannot find Primetime at $(PRIMETIME_POWER_BIN))
+endif
+
+VCD2SAIF_BIN = $(SYNOPSYS_HOME)/syn/$(DC_VERSION)/bin/vcd2saif
+ifeq ($(wildcard $(VCD2SAIF_BIN)),)
+$(error Cannot find vcd2saif at $(VCD2SAIF_BIN))
+endif
+
+VPD2VCD_BIN = $(SYNOPSYS_HOME)/vcs/$(VCS_VERSION)/bin/vpd2vcd
+ifeq ($(wildcard $(VPD2VCD_BIN)),)
+$(error Cannot find vpd2vcd at $(VPD2VCD_BIN))
+endif
