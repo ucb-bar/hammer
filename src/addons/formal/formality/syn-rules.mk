3f5d6c542a8d6ad147f65e1ff59fd57a3bbe9ce5
Edward Wang <edward.c.wang@compdigitec.com>
2017-06-29 12:35:07 -0700

    TECHNOLOGY_VERILOG_FILES -> TECHNOLOGY_VERILOG_SIMULATION_FILES

--- src/addons/formal/formality/syn-rules.mk
+++ src/addons/formal/formality/syn-rules.mk
@@ -6,7 +6,7 @@ $(SIGNOFF_SYN_FORMAL_DIR)/formality-$(MAP_TOP)-$(SYN_TOP).out: \
 		$(SYN_FORMAL_ADDON)/run-formality \
 		$(OBJ_MAP_RTL_V) \
 		$(OBJ_SYN_MAPPED_V) \
-		$(TECHNOLOGY_VERILOG_FILES) \
+		$(TECHNOLOGY_VERILOG_SIMULATION_FILES) \
 		$(TECHNOLOGY_CCS_LIBRARY_FILES)
 	@mkdir -p $(dir $@)
 	$(SCHEDULER_CMD) -- $(CMD_PTEST) --test $(abspath $<) --out $(abspath $@) --args $(abspath $(FORMALITY_BIN)) --dc-dir $(abspath $(OBJ_SYN_DIR)/synopsys-dc-workdir) $(if $(FORMALITY_NO_VERIFY),--no-verify)
