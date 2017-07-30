051822092cd60bdbdd9ce3a640b5b94245b0a9e4
Donggyu Kim <dgkim@eecs.berkeley.edu>
2017-07-30 01:22:35 -0700

    selectively map macros

---
+++ src/addons/map/none/rules.mk
@@ -0,0 +1,7 @@
+$(OBJ_MAP_DIR)/plsi-generated/$(MAP_TOP).macros_for_synthesis.v:
+	mkdir -p $(dir $@)
+	touch $@
+
+$(OBJ_MAP_DIR)/plsi-generated/$(MAP_TOP).macros_for_synthesis.mk:
+	mkdir -p $(dir $@)
+	touch $@
