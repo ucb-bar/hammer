11f42fd70d6a1b3500977152722e6616d8077c65
Edward Wang <edward.c.wang@compdigitec.com>
2017-11-07 22:45:27 -0800

    Add TimeValue

--- src/hammer-vlsi/test.py
+++ src/hammer-vlsi/test.py
@@ -139,5 +139,44 @@ class HammerToolTest(unittest.TestCase):
         # Cleanup
         shutil.rmtree(test.run_dir)
 
+class TimeValueTest(unittest.TestCase):
+    def test_read_and_write(self):
+        """
+        Test that we can parse and emit time values.
+        """
+        tv = hammer_vlsi.TimeValue("1000 ns")
+        self.assertEqual(tv.str_value_in_units("ns"), "1000 ns")
+        self.assertEqual(tv.str_value_in_units("us"), "1 us")
+        self.assertEqual(tv.value_in_units("ps"), 1000000.0)
+
+    def test_default_prefix(self):
+        """
+        Test that we can parse and emit time values.
+        """
+        tv = hammer_vlsi.TimeValue("1000")
+        self.assertEqual(tv.value_in_units("ns"), 1000)
+        tv2 = hammer_vlsi.TimeValue("42", "m")
+        self.assertEqual(tv2.value_in_units("ms"), 42)
+
+    def test_errors(self):
+        """
+        Test that errors get caught.
+        """
+        def bad_1():
+            hammer_vlsi.TimeValue("bugaboo")
+        def bad_2():
+            hammer_vlsi.TimeValue("1.1.1.1 ps")
+        def bad_3():
+            hammer_vlsi.TimeValue("420 xs")
+        def bad_4():
+            hammer_vlsi.TimeValue("12 noobs")
+        def bad_5():
+            hammer_vlsi.TimeValue("666......")
+        self.assertRaises(ValueError, bad_1)
+        self.assertRaises(ValueError, bad_2)
+        self.assertRaises(ValueError, bad_3)
+        self.assertRaises(ValueError, bad_4)
+        self.assertRaises(ValueError, bad_5)
+
 if __name__ == '__main__':
     unittest.main()
