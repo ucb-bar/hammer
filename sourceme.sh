0651d6fadd6920e36b2de8d15d111adb75d21446
Edward Wang <edward.c.wang@compdigitec.com>
2017-11-21 16:22:12 -0800

    Check in helper shell frag

---
+++ sourceme.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Convenience script for setting variables for in-tree use of hammer.
+if [ -z "$HAMMER_HOME" ]; then
+    >&2 echo "Must set HAMMER_HOME to root. Try export HAMMER_HOME="\$PWD" for the current dir."
+    exit 1
+fi
+
+export HAMMER_VLSI="$HAMMER_HOME/src/hammer-vlsi"
+export PYTHONPATH="$HAMMER_HOME/src:$HAMMER_HOME/src/jsonschema:$HAMMER_HOME/src/python-jsonschema-objects:$HAMMER_HOME/src/hammer-tech:$HAMMER_HOME/src/hammer-vlsi:$HAMMER_HOME/src/tools/pyyaml/lib3:$PYTHONPATH"
+export PATH="$HAMMER_HOME/src/hammer-shell:$PATH"
