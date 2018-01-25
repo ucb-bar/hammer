c51de68618cad20dd7670a50b61d3cec99f4d588
Edward Wang <edward.c.wang@compdigitec.com>
2018-01-25 00:34:01 -0800

    Use -a for coverage to capture both runs

--- src/test/coverage.sh
+++ src/test/coverage.sh
@@ -2,8 +2,10 @@
 
 set -ex
 
-coverage3 run ../hammer-vlsi/test.py
+coverage3 erase
 
-coverage3 run ../hammer_config_test/test.py
+coverage3 run -a ../hammer-vlsi/test.py
+
+coverage3 run -a ../hammer_config_test/test.py
 
 coverage3 html
