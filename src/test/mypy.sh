d9aea4d004cfa6db3782f923f2562142db600c8e
Edward Wang <edward.c.wang@compdigitec.com>
2018-04-13 22:09:58 -0700

    mypy: add mocksynth

--- src/test/mypy.sh
+++ src/test/mypy.sh
@@ -13,6 +13,7 @@ mypy ../hammer_config_test/test.py
 # Plugins
 mypy ../hammer-vlsi/synthesis/dc/__init__.py
 mypy ../hammer-vlsi/synthesis/genus/__init__.py
+mypy ../hammer-vlsi/synthesis/mocksynth/__init__.py
 mypy ../hammer-vlsi/par/icc/__init__.py
 mypy ../hammer-vlsi/par/innovus/__init__.py
 mypy ../hammer-vlsi/par/*.py
