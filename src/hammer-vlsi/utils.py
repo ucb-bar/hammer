be4adb3fd4a6ebeb7f7b612ce397c643398bae3e
Edward Wang <edward.c.wang@compdigitec.com>
2018-04-20 10:05:17 -0700

    Move general util methods out of HammerTool

--- src/hammer-vlsi/utils.py
+++ src/hammer-vlsi/utils.py
@@ -7,6 +7,7 @@
 #  Copyright 2018 Edward Wang <edward.c.wang@compdigitec.com>
 
 import copy
+from typing import List, Any, Set
 
 
 def deepdict(x: dict) -> dict:
@@ -28,3 +29,29 @@ def deeplist(x: list) -> list:
     :return: Deep copy of the list provided by copy.deepcopy().
     """
     return copy.deepcopy(x)
+
+
+def add_lists(a: List[str], b: List[str]) -> List[str]:
+    """Helper method: join two lists together while type checking."""
+    assert isinstance(a, List)
+    assert isinstance(b, List)
+    return a + b
+
+
+def in_place_unique(items: List[Any]) -> None:
+    """
+    "Fast" in-place uniquification of a list.
+    :param items: List to be uniquified.
+    """
+    seen = set()  # type: Set[Any]
+    i = 0
+    # We will be all done when i == len(items)
+    while i < len(items):
+        item = items[i]
+        if item in seen:
+            # Delete and keep index pointer the same.
+            del items[i]
+            continue
+        else:
+            seen.add(item)
+            i += 1
