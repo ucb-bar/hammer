5089ff3b132e2f38c0676ec654dd006f20e3119d
Edward Wang <edward.c.wang@compdigitec.com>
2018-05-07 00:38:40 -0700

    Fix some more minor errors

--- src/hammer_config/config_src.py
+++ src/hammer_config/config_src.py
@@ -440,10 +440,10 @@ def combine_configs(configs: Iterable[dict]) -> dict:
 
 def load_config_from_paths(config_paths: Iterable[str], strict: bool = False) -> List[dict]:
     """
-    Load configuration from paths containing *.yml and *.json files.
+    Load configuration from paths containing \*.yml and \*.json files.
     As noted in README.config, .json will take precedence over .yml files.
 
-    :param config_paths: Path to *.yml and *.json config files.
+    :param config_paths: Path to \*.yml and \*.json config files.
     :param strict: Set to true to error if the file is not found.
     :return: A list of configs in increasing order of precedence.
     """
