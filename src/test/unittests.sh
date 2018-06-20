3d34e45d2decce7b9214042249a3e631e323d88f
Edward Wang <edward.c.wang@compdigitec.com>
2018-06-20 11:31:36 -0700

    Dump intermediate configs in syn_par

--- src/test/unittests.sh
+++ src/test/unittests.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 python3 ../hammer-vlsi/test.py
+python3 ../hammer-vlsi/cli_driver_test.py
 python3 ../hammer-vlsi/utils_test.py
 python3 ../hammer-vlsi/verilog_utils_test.py
 python3 ../hammer_config_test/test.py
