277deb052fcb533b79178f772f6d161fe770af48
Colin Schmidt <colins@eecs.berkeley.edu>
2018-06-21 13:18:01 -0700

    Add generated clocks to clock API

--- src/hammer-vlsi/hammer_vlsi/hammer_vlsi_impl.py
+++ src/hammer-vlsi/hammer_vlsi/hammer_vlsi_impl.py
@@ -783,8 +783,11 @@ class HasSDCSupport(HammerTool):
         clocks = self.get_clock_ports()
         for clock in clocks:
             # TODO: FIXME This assumes that library units are always in ns!!!
-            if clock.port is not None:
-                output.append("create_clock {0} -name {1} -period {2}".format(clock.port, clock.name, clock.period.value_in_units("ns")))
+            if clock.generated is not None:
+                output.append("create_generated_clock -name {n} -source {m_path} -divide_by {div} {path}".
+                        format(n=clock.name, m_path=clock.source_path, div=clock.divisor, path=clock.path))
+            elif clock.path is not None:
+                output.append("create_clock {0} -name {1} -period {2}".format(clock.path, clock.name, clock.period.value_in_units("ns")))
             else:
                 output.append("create_clock {0} -name {0} -period {1}".format(clock.name, clock.period.value_in_units("ns")))
             if clock.uncertainty is not None:
