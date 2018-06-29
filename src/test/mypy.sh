5198dc4c020fc37de5e990debd52fce6691211b6
Edward Wang <edward.c.wang@compdigitec.com>
2018-06-28 18:07:34 -0700

    Simplify mypy.sh script and make certain plugins optional to check

--- src/test/mypy.sh
+++ src/test/mypy.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
 set -x
 # Core
-mypy ../hammer-vlsi/cli_driver.py
+mypy --package hammer_vlsi
 
 # Shell
 mypy ../hammer-shell/get-config
 
 # Testing code
-mypy ../hammer-vlsi/test.py
-mypy ../hammer-vlsi/cli_driver_test.py
-mypy ../hammer-vlsi/utils_test.py
-mypy ../hammer-vlsi/verilog_utils_test.py
+mypy ../hammer-vlsi/*test.py
 mypy ../hammer_config_test/test.py 
 
 # Plugins
-mypy ../hammer-vlsi/synthesis/dc/__init__.py
-mypy ../hammer-vlsi/synthesis/genus/__init__.py
 mypy ../hammer-vlsi/synthesis/mocksynth/__init__.py
-mypy ../hammer-vlsi/par/icc/__init__.py
-mypy ../hammer-vlsi/par/innovus/__init__.py
-mypy ../hammer-vlsi/par/*.py
+mypy ../hammer-vlsi/par/nop.py
+
+# Plugins which may or may not exist
+if [ -f ../hammer-vlsi/synthesis/dc/__init__.py ]; then
+	mypy ../hammer-vlsi/synthesis/dc/__init__.py
+fi
+if [ -f ../hammer-vlsi/synthesis/genus/__init__.py ]; then
+	mypy ../hammer-vlsi/synthesis/genus/__init__.py
+fi
+if [ -f ../hammer-vlsi/par/icc/__init__.py ]; then
+	mypy ../hammer-vlsi/par/icc/__init__.py
+fi
+if [ -f ../hammer-vlsi/par/innovus/__init__.py ]; then
+	mypy ../hammer-vlsi/par/innovus/__init__.py
+fi
