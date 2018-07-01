d14ebd2b8c4feba2fd33de453d1274145a420441
Edward Wang <edward.c.wang@compdigitec.com>
2018-07-01 00:49:06 -0700

    Newline test

--- src/test/unittests.sh
+++ src/test/unittests.sh
@@ -3,4 +3,5 @@ python3 ../hammer-vlsi/test.py
 python3 ../hammer-vlsi/cli_driver_test.py
 python3 ../hammer-vlsi/utils_test.py
 python3 ../hammer-vlsi/verilog_utils_test.py
+
 python3 ../hammer_config_test/test.py
