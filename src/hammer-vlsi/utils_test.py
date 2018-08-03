989dd4092ec919fc9c46d54fc59fef4372f19608
Edward Wang <edward.c.wang@compdigitec.com>
2018-08-03 00:34:34 -0400

    Add get_or_else

--- src/hammer-vlsi/utils_test.py
+++ src/hammer-vlsi/utils_test.py
@@ -7,7 +7,7 @@
 
 from typing import Dict, Tuple, List
 
-from hammer_utils import topological_sort
+from hammer_utils import topological_sort, get_or_else
 
 import unittest
 
@@ -31,6 +31,12 @@ class UtilsTest(unittest.TestCase):
         }  # type: Dict[str, Tuple[List[str], List[str]]]
 
         self.assertEqual(topological_sort(graph, ["1", "2", "3"]), ["1", "2", "3", "4", "6", "7", "5", "8"])
+
+    def test_get_or_else(self) -> None:
+        self.assertEqual(get_or_else(None, "default"), "default")
+        self.assertEqual(get_or_else(None, ""), "")
+        self.assertEqual(get_or_else("Hello World", "default"), "Hello World")
+        self.assertEqual(get_or_else("Hello World", ""), "Hello World")
 
 
 if __name__ == '__main__':
