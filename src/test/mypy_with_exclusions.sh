12f5d5166c5d0baa082877165125d40abc574d75
Edward Wang <edward.c.wang@compdigitec.com>
2018-09-15 19:59:54 -0400

    Ignore TechJSON warning

--- src/test/mypy_with_exclusions.sh
+++ src/test/mypy_with_exclusions.sh
@@ -2,4 +2,4 @@
 # Run mypy.sh but exclude bits known to be problematic to mypy.
 # Also, sort and remove duplicates from multiple calls to mypy.
 
-./mypy.sh | grep -v "python-jsonschema-objects" | grep -v installs | grep -v tarballs | grep -v ccs | grep -v nldm | grep -v supplies | grep -v lef_file | grep -v qrc_techfile | grep -v serialize | grep -v "hammer_tech.Library" | grep -v milkyway_ | grep -v tluplus | grep -v jsonschema | grep -v "pyyaml/" | grep -v provides | grep -v \"libraries\" | sort -u
+./mypy.sh | grep -v "python-jsonschema-objects" | grep -v TechJSON | grep -v installs | grep -v tarballs | grep -v ccs | grep -v nldm | grep -v supplies | grep -v lef_file | grep -v qrc_techfile | grep -v serialize | grep -v "hammer_tech.Library" | grep -v milkyway_ | grep -v tluplus | grep -v jsonschema | grep -v "pyyaml/" | grep -v provides | grep -v \"libraries\" | sort -u
