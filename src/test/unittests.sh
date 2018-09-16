bd514c0d2658dff9134b786d8a3ea244f0967c5b
Edward Wang <edward.c.wang@compdigitec.com>
2018-09-16 12:12:22 -0400

    Split ValueWithUnits tests into a new file

--- src/test/unittests.sh
+++ src/test/unittests.sh
@@ -8,6 +8,7 @@ trap 'err=1' ERR
 python3 ../hammer-vlsi/test.py
 python3 ../hammer-vlsi/cli_driver_test.py
 python3 ../hammer-vlsi/utils_test.py
+python3 ../hammer-vlsi/units_test.py
 python3 ../hammer-vlsi/verilog_utils_test.py
 python3 ../hammer_config_test/test.py
 
