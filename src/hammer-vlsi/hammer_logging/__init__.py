b6809859f13ef8392d4b36a7fe2158c969510a12
Edward Wang <edward.c.wang@compdigitec.com>
2018-10-10 11:41:27 -0700

    Refactor hammer_logging module into a folder

---
+++ src/hammer-vlsi/hammer_logging/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  Hammer logging code.
+#
+#  Copyright 2018 Edward Wang <edward.c.wang@compdigitec.com>
+
+__all__ = ['HammerVLSIFileLogger', 'HammerVLSILogging', 'HammerVLSILoggingContext', 'Level']
+
+from .logging import HammerVLSIFileLogger, HammerVLSILogging, HammerVLSILoggingContext, Level
