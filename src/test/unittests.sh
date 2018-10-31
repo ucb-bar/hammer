fdd3652a81fddd515efdb6d88ff2e045fe782133
Edward Wang <edward.c.wang@compdigitec.com>
2018-10-30 17:16:38 -0700

    Add LEF utils

--- src/test/unittests.sh
+++ src/test/unittests.sh
@@ -10,6 +10,7 @@ python3 ../hammer-vlsi/cli_driver_test.py
 python3 ../hammer-vlsi/utils_test.py
 python3 ../hammer-vlsi/units_test.py
 python3 ../hammer-vlsi/verilog_utils_test.py
+python3 ../hammer-vlsi/lef_utils_test.py
 python3 ../hammer_config_test/test.py
 
 test $err = 0 # Return non-zero if any command failed
