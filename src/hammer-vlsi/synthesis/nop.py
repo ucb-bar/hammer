2f052adda323fa4bd5309d8accdef97674942247
Edward Wang <edward.c.wang@compdigitec.com>
2018-11-05 07:11:37 -0800

    Run linter on nop synthesis

--- src/hammer-vlsi/synthesis/nop.py
+++ src/hammer-vlsi/synthesis/nop.py
@@ -6,14 +6,14 @@
 #
 #  Copyright 2018 Edward Wang <edward.c.wang@compdigitec.com>
 
-from typing import List
-
 from hammer_vlsi import HammerSynthesisTool, DummyHammerTool
 from hammer_utils import deeplist
+
 
 class NopSynth(HammerSynthesisTool, DummyHammerTool):
     def fill_outputs(self) -> bool:
         self.output_files = deeplist(self.input_files)
         return True
+
 
 tool = NopSynth
