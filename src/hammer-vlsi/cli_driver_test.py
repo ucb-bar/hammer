3e754b1cc3912d03c9bef5fab5bbe42accac524d
Colin Schmidt <colins@eecs.berkeley.edu>
2019-02-08 08:23:03 -0800

    Add hierarchical unit test

--- src/hammer-vlsi/cli_driver_test.py
+++ src/hammer-vlsi/cli_driver_test.py
@@ -14,7 +14,7 @@ from typing import Any, Callable, Dict, Optional
 import hammer_config
 from hammer_logging.test import HammerLoggingCaptureContext
 from hammer_tech import MacroSize
-from hammer_vlsi import CLIDriver, HammerDriver
+from hammer_vlsi import CLIDriver, HammerDriver, PlacementConstraint
 from hammer_utils import deepdict
 
 import unittest
@@ -323,6 +323,54 @@ class CLIDriverTest(unittest.TestCase):
         with open(config_packed_path, "r") as f:
             packed_output = json.loads(f.read())
         self.assertEqual(packed_output, dumped_output)
+
+        # Cleanup
+        shutil.rmtree(syn_rundir)
+
+    def test_hier_dump(self) -> None:
+        """
+        Test that hierarchical settings work properly.
+        """
+        # Set up some temporary folders for the unit test.
+        syn_rundir = tempfile.mkdtemp()
+
+        # Generate a config for testing.
+        top_module = "dummy"
+        config_path = os.path.join(syn_rundir, "run_config.json")
+        def add_hier(d: Dict[str, Any]) -> Dict[str, Any]:
+            output = deepdict(d)
+            dummyPlacement = PlacementConstraint.from_dict({"path":"dummy","type":"dummy","x":"0","y":"0","width":"0","height":"0"}).to_dict()
+            output["vlsi.inputs.default_output_load"] = 1
+            output["vlsi.inputs.hierarchical.top_module"] = top_module
+            output["vlsi.inputs.hierarchical.flat"] = "hierarchical"
+            output["vlsi.inputs.hierarchical.config_source"] = "manual"
+            output["vlsi.inputs.hierarchical.manual_modules"] = [{"mod1": ["m1s1", "m1s2"], "mod2": ["m2s1"], top_module: ["mod1", "mod2"]}]
+            output["vlsi.inputs.hierarchical.manual_placement_constraints"] = [{"mod1": [dummyPlacement]}, {"mod2": [dummyPlacement]}, {"m1s1": [dummyPlacement]},
+                    {"m1s2": [dummyPlacement]}, {"m2s1": [dummyPlacement]}, {top_module: [dummyPlacement]}]
+            output["vlsi.inputs.hierarchical.constraints"] = [{"mod1": [{"vlsi.inputs.default_output_load": 2}]},
+                    {"m2s1": [{"vlsi.inputs.default_output_load": 3}]}]
+            return output
+        self.generate_dummy_config(syn_rundir, config_path, top_module, postprocessing_func=add_hier)
+
+        # Check that running the CLIDriver executes successfully (code 0).
+        with self.assertRaises(SystemExit) as cm:  # type: ignore
+            CLIDriver().main(args=[
+                "auto",  # action
+                "-p", config_path,
+                "--obj_dir", syn_rundir
+            ])
+        self.assertEqual(cm.exception.code, 0)
+
+        # Check that dumped output should be same as what we read in.
+        with open(os.path.join(syn_rundir, "syn-m2s1/full_config.json"), "r") as f:
+            dumped_output = json.loads(f.read())
+        self.assertEqual(dumped_output['vlsi.inputs.default_output_load'], 3)
+        with open(os.path.join(syn_rundir, "syn-m1s1/full_config.json"), "r") as f:
+            dumped_output = json.loads(f.read())
+        self.assertEqual(dumped_output['vlsi.inputs.default_output_load'], 1)
+        with open(os.path.join(syn_rundir, "syn-mod1/full_config.json"), "r") as f:
+            dumped_output = json.loads(f.read())
+        self.assertEqual(dumped_output['vlsi.inputs.default_output_load'], 2)
 
         # Cleanup
         shutil.rmtree(syn_rundir)
