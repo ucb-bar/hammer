56ea61e723dc1b273afb2fea09da8f6952bd561c
Colin Schmidt <colins@eecs.berkeley.edu>
2019-05-13 10:37:13 -0700

    Add punch-through sdc API
    Fixes #323

--- src/hammer-vlsi/hammer_vlsi/hammer_vlsi_impl.py
+++ src/hammer-vlsi/hammer_vlsi/hammer_vlsi_impl.py
@@ -1018,6 +1018,11 @@ class HasSDCSupport(HammerTool):
                 direction=delay.direction,
                 name=delay.name
             ))
+
+        # Custom pin constraints that are verbatim appended
+        for custom in self.get_setting("vlsi.inputs.custom_sdc_constraints"):
+            output.append(str(custom))
+
         return "\n".join(output)
 
     @property
