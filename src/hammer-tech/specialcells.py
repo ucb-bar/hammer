4948551dd63a535af76c7bbc4a8c9722e5fc26ca
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2019-05-23 11:52:32 +0200

    Add special cell data struct
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

---
+++ src/hammer-tech/specialcells.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  stackup.py
+#  Data structures to represent technology's special cells.
+#
+#  See LICENSE for licence details.
+
+from enum import Enum
+from typing import List, NamedTuple, Tuple, Dict, Optional
+from decimal import Decimal
+
+class CellType(Enum):
+    TieCell = 1
+    EndCap = 2
+    IOFiller = 3
+    StdFiller = 4
+
+    @classmethod
+    def __mapping(cls) -> Dict[str, "RoutingDirection"]:
+        return {
+            "tiecell": CellType.TieCell,
+            "endcap": CellType.EndCap,
+            "iofiller": CellType.IOFiller,
+            "stdfiller": CellType.StdFiller,
+        }
+
+    @staticmethod
+    def from_str(input_str: str) -> "CellType":
+        # pylint: disable=missing-docstring
+        try:
+            return CellType.__mapping()[input_str]
+        except KeyError:
+            raise ValueError("Invalid CellType: " + str(input_str))
+
+    def __str__(self) -> str:
+        return reverse_dict(RoutingDirection.__mapping())[self]
+
+
+class SpecialCell(NamedTuple('SpecialCell', [
+        ('cell_type', CellType),
+        ('name', str),
+        ('size', Optional[Decimal])
+])):
+    """
+    A SpecialCell is a technology cell used for non logic task (e.g. Tiecells,
+    Endcap, filler, etc.
+
+    """
+    __slots__ = ()
+
+    @staticmethod
+    def from_setting(d: dict) -> "SpecialCell":
+        size = d['size']
+        if size is not None:
+            size = Decimal(d['size'])
+        # pylint: disable=missing-docstring
+        return SpecialCell(
+            cell_type=CellType.from_str(d["cell_type"]),
+            name=str(d["name"]),
+            size=size,
+        )
