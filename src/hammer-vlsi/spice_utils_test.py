73bce8e6368fe63d34ad79be831a0769bcb01a3d
John Wright <johnwright@eecs.berkeley.edu>
2019-06-14 01:27:11 -0700

    missed some types

--- src/hammer-vlsi/spice_utils_test.py
+++ src/hammer-vlsi/spice_utils_test.py
@@ -6,7 +6,7 @@
 #  See LICENSE for licence details.
 
 from inspect import cleandoc
-from typing import Iterable, Tuple, Any
+from typing import Iterable, Tuple, Any, Dict, List
 
 from hammer_utils import SpiceUtils
 
