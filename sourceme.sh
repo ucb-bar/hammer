77bf7ea4d36a7ad6801d405a23fd3b4b300ec782
Jiuyang liu <liujiuyang1994@gmail.com>
2019-06-15 10:31:17 +0000

    import different 3rd lib one by one.

--- sourceme.sh
+++ sourceme.sh
@@ -6,9 +6,17 @@ else
 export HAMMER_VLSI="$HAMMER_HOME/src/hammer-vlsi"
 export PYTHONPATH="$HAMMER_HOME/src:$HAMMER_HOME/src/hammer-tech:$HAMMER_HOME/src/hammer-vlsi:$PYTHONPATH"
 # if import failed, then use vending version
-python3 -c "import jsonschema, python_jsonschema_objects, yaml" > /dev/null 2>&1
+python3 -c "import jsonschema" > /dev/null 2>&1
 if [[ $? == 1 ]]; then
-    export PYTHONPATH="$HAMMER_HOME/src/contrib/jsonschema:$HAMMER_HOME/src/contrib/python-jsonschema-objects:$HAMMER_HOME/src/contrib/pyyaml:$PYTHONPATH"
+    export PYTHONPATH="$HAMMER_HOME/src/contrib/jsonschema:$PYTHONPATH"
+fi
+python3 -c "import python_jsonschema_objects" > /dev/null 2>&1
+if [[ $? == 1 ]]; then
+    export PYTHONPATH="$HAMMER_HOME/src/contrib/python-jsonschema-objects:$PYTHONPATH"
+fi
+python3 -c "import yaml" > /dev/null 2>&1
+if [[ $? == 1 ]]; then
+    export PYTHONPATH="$HAMMER_HOME/src/contrib/pyyaml:$PYTHONPATH"
 fi
 export MYPYPATH="$PYTHONPATH"
 export PATH="$HAMMER_HOME/src/hammer-shell:$PATH"
