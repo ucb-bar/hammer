e144f03ef66f546f9ddcf9a5eedab2fed52eceb8
Harrison Liew <harrisonliew@gmail.com>
2019-09-06 14:18:13 -0700

    hotfix script plumbing, GDS layermap

--- src/hammer-tech/hammer_tech.py
+++ src/hammer-tech/hammer_tech.py
@@ -717,6 +717,23 @@ class HammerTechnology:
                             tarfile.open(file).extractall(path=os.path.join(root, f + "_dir"))
                             os.remove(file)
                             os.renames(os.path.join(root, f + "_dir"), file)
+                # Source any hotfix script when tarball is extracted the first time
+                self.source_tech_hotfix()
+
+    def source_tech_hotfix(self) -> None:
+        """Source a script to apply any needed hotfixes to technology libraries."""
+        if not self.has_setting("technology.core.hotfix_script"):
+            # If the key doesn't exist we can safely say there are no fixes needed to be applied
+            return
+
+        hotfix = os.path.join(self.path, self.get_setting("technology.core.hotfix_script"))
+        if not os.path.exists(hotfix):
+            self.logger.error("hotfix script does not exist; libraries in your tech cache may not work properly")
+            return
+        else:
+            # TODO: execute this script differently depending on script type. Assumed to be shell for now.
+            self.logger.debug("Applying hotfix script to {} libaries".format(self.name))
+            os.system("cd {dir} && source {script}".format(dir=self.extracted_tarballs_dir, script=hotfix))
 
     def get_extra_libraries(self) -> List[ExtraLibrary]:
         """
