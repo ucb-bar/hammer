381d2e8382017d2262e661c5418813a9bfbb9a64
Harrison Liew <harrisonliew@gmail.com>
2019-09-10 16:43:14 -0700

    pythonpath

--- sourceme.sh
+++ sourceme.sh
@@ -4,7 +4,7 @@ if [ -z "$HAMMER_HOME" ]; then
     >&2 echo "Must set HAMMER_HOME to root. Try export HAMMER_HOME="\$PWD" for the current dir."
 else
 export HAMMER_VLSI="$HAMMER_HOME/src/hammer-vlsi"
-export PYTHONPATH="$HAMMER_HOME/src:$HAMMER_HOME/src/jsonschema:$HAMMER_HOME/src/python-jsonschema-objects:$HAMMER_HOME/src/hammer-tech:$HAMMER_HOME/src/hammer-vlsi:$HAMMER_HOME/src/tools/pyyaml/lib3:$PYTHONPATH"
+export PYTHONPATH="$HAMMER_HOME/src:$HAMMER_HOME/src/jsonschema:$HAMMER_HOME/src/python-jsonschema-objects:$HAMMER_HOME/src/hammer-tech:$HAMMER_HOME/src/hammer-vlsi:$HAMMER_HOME/src/tools/pyyaml/lib3:$HAMMER_HOME/src/tools/python-gdsii:$PYTHONPATH"
 export MYPYPATH="$PYTHONPATH"
 export PATH="$HAMMER_HOME/src/hammer-shell:$PATH"
 fi
