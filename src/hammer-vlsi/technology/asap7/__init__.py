e30da8cc55297db0d6fe28cfe3309f77450944c0
Harrison Liew <harrisonliew@gmail.com>
2019-10-03 15:05:25 -0700

    Take care of case where blackboxes use the 'blk' layer for abstract extraction

--- src/hammer-vlsi/technology/asap7/__init__.py
+++ src/hammer-vlsi/technology/asap7/__init__.py
@@ -149,14 +149,22 @@ except ImportError:
 # load the standard cell list from the gds folder and lop off '_SL' from end
 cell_list = \[line.strip()\[:-3\] for line in open('{cell_list_file}', 'r')\]
 
+# Need to remove blk layer from any macros, else LVS rule deck interprets it as a polygon
+blockage_datatype = 4
+
 # load original_gds
 gds_lib = gdspy.GdsLibrary().read_gds(infile='{gds_file}', units='import')
 # Iterate through cells that aren't part of standard cell library and scale
 for k,v in gds_lib.cell_dict.items():
     if not any(cell in k for cell in cell_list):
         print('Scaling down ' + k)
-        for poly in v.polygons:
-            poly.scale(0.25)
+
+        # Need to remove 'blk' layer from any macros, else LVS rule deck interprets it as a polygon
+        # This has a layer datatype of 4
+        # Then scale down the polygon
+        v.polygons = \[poly.scale(0.25) for poly in v.polygons if not 4 in poly.datatypes\]
+
+        # Scale paths
         for path in v.paths:
             path.scale(0.25)
             # gdspy bug: we also need to scale custom path extensions
@@ -164,6 +172,8 @@ for k,v in gds_lib.cell_dict.items():
             for i, end in enumerate(path.ends):
                 if isinstance(end, tuple):
                     path.ends\[i\] = tuple(\[e*0.25 for e in end\])
+
+        # Scale and move labels
         for label in v.labels:
             # Bug fix for some EDA tools that didn't set MAG field in gds file
             # Maybe this is expected behavior in ASAP7 PDK
@@ -171,6 +181,7 @@ for k,v in gds_lib.cell_dict.items():
             label.magnification = 0.25
             label.translate(-label.position\[0\]*0.75, -label.position\[1\]*0.75)
 
+        # Scale and move references
         for ref in v.references:
             ref.magnification = 0.25
             ref.translate(-ref.origin\[0\]*0.75, -ref.origin\[1\]*0.75)
