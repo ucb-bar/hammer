2c08312ea5d5d5d9ee8df052c47e28dfad6f490f
Harrison Liew <harrisonliew@gmail.com>
2020-02-06 10:51:35 -0800

    optionally type check plugins and fix all_regs types

--- src/test/mypy_with_exclusions.sh
+++ src/test/mypy_with_exclusions.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # Run mypy.sh but sorts and removes duplicates from multiple calls to mypy.
+# Pass the technology name as the first argument if you also want to type check the hammer-<tech>-plugin.
 
-./mypy.sh > _stdout.txt
+./mypy.sh "$@" > _stdout.txt
 mypy_status=$?
 
 cat _stdout.txt | sort -u
