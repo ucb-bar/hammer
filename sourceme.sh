2d8941cdb6ca29c3460705e36b73cd6d843a834c
Arya Reais-Parsi <himself@aryaparsi.com>
2020-06-09 18:28:29 -0700

    sourceme.sh finds its root directory to set HAMMER_HOME if unset

--- sourceme.sh
+++ sourceme.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 # Convenience script for setting variables for in-tree use of hammer.
-if [ -z "$HAMMER_HOME" ]; then
-    >&2 echo "Must set HAMMER_HOME to root. Try export HAMMER_HOME="\$PWD" for the current dir."
+
+if [ -z "${HAMMER_HOME}" ]; then
+  # Try to find the location of the script even if it's called through a symlink.
+  #  https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "${SOURCE}" ]; do
+    DIR="$( cd -P "$( dirname "${SOURCE}" )" > /dev/null 2>&1 && pwd )"
+    SOURCE="$( readlink "${SOURCE}")"
+    [[ ${SOURCE} != /* ]] && SOURCE="${DIR}/${SOURCE}"
+  done
+  DIR="$( cd -P "$( dirname "${SOURCE}" )" > /dev/null 2>&1 && pwd )"
+
+  export HAMMER_HOME="${DIR}"
+  >&2 echo "Using (default) HAMMER_HOME=${HAMMER_HOME}"
 else
-export HAMMER_VLSI="$HAMMER_HOME/src/hammer-vlsi"
-export PYTHONPATH="$HAMMER_HOME/src:$HAMMER_HOME/src/jsonschema:$HAMMER_HOME/src/python-jsonschema-objects:$HAMMER_HOME/src/hammer-tech:$HAMMER_HOME/src/hammer-vlsi:$HAMMER_HOME/src/tools/pyyaml/lib3:$PYTHONPATH"
-export MYPYPATH="$PYTHONPATH"
-export PATH="$HAMMER_HOME/src/hammer-shell:$PATH"
+  >&2 echo "Using HAMMER_HOME=${HAMMER_HOME}"
 fi
+
+export HAMMER_VLSI="${HAMMER_HOME}/src/hammer-vlsi"
+export PYTHONPATH="${HAMMER_HOME}/src:${HAMMER_HOME}/src/jsonschema:${HAMMER_HOME}/src/python-jsonschema-objects:${HAMMER_HOME}/src/hammer-tech:${HAMMER_HOME}/src/hammer-vlsi:${HAMMER_HOME}/src/tools/pyyaml/lib3:${PYTHONPATH}"
+export MYPYPATH="${PYTHONPATH}"
+export PATH="${HAMMER_HOME}/src/hammer-shell:${PATH}"
