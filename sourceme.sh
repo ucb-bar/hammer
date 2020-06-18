275f7e14e143755f3d0e7e089509ccdb3af69419
Arya Reais-Parsi <himself@aryaparsi.com>
2020-06-18 14:04:46 -0700

    Make sourceme.sh set HAMMER_HOME to its root dir by default (#583)
    * sourceme.sh finds its root directory to set HAMMER_HOME if unset
    * simplify symlink resolution using linux readlink -f switch; keep non-POSIX BASH_SOURCE[0] so that sourcing the script works
    * make echo statement slightly more informative
    * make dependence on /bin/bash explicit - /bin/sh symlinks to /bin/bash on systems we seem to care about
    * remove comment when HAMMER_HOME already set

--- sourceme.sh
+++ sourceme.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 # Convenience script for setting variables for in-tree use of hammer.
-if [ -z "$HAMMER_HOME" ]; then
-    >&2 echo "Must set HAMMER_HOME to root. Try export HAMMER_HOME="\$PWD" for the current dir."
-else
-export HAMMER_VLSI="$HAMMER_HOME/src/hammer-vlsi"
-export PYTHONPATH="$HAMMER_HOME/src:$HAMMER_HOME/src/jsonschema:$HAMMER_HOME/src/python-jsonschema-objects:$HAMMER_HOME/src/hammer-tech:$HAMMER_HOME/src/hammer-vlsi:$HAMMER_HOME/src/tools/pyyaml/lib3:$PYTHONPATH"
-export MYPYPATH="$PYTHONPATH"
-export PATH="$HAMMER_HOME/src/hammer-shell:$PATH"
+
+if [ -z "${HAMMER_HOME}" ]; then
+  # Try to find the location of the script even if it's called through a symlink.
+  #  https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+  #  https://stackoverflow.com/questions/35006457/choosing-between-0-and-bash-source
+  export HAMMER_HOME="$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )"
+  >&2 echo "Setting HAMMER_HOME=${HAMMER_HOME}"
 fi
+
+export HAMMER_VLSI="${HAMMER_HOME}/src/hammer-vlsi"
+export PYTHONPATH="${HAMMER_HOME}/src:${HAMMER_HOME}/src/jsonschema:${HAMMER_HOME}/src/python-jsonschema-objects:${HAMMER_HOME}/src/hammer-tech:${HAMMER_HOME}/src/hammer-vlsi:${HAMMER_HOME}/src/tools/pyyaml/lib3:${PYTHONPATH}"
+export MYPYPATH="${PYTHONPATH}"
+export PATH="${HAMMER_HOME}/src/hammer-shell:${PATH}"
