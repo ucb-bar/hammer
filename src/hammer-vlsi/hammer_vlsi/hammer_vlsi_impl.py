73afa52fdd3a2c2cd13ba1f5362ebee6bfd5054c
Jerry Zhao <jerryz123@berkeley.edu>
2021-04-02 10:42:07 -0700

    Use len(clocks) instead of len(grouped_clocks) + len(ungrouped_clocks)

--- src/hammer-vlsi/hammer_vlsi/hammer_vlsi_impl.py
+++ src/hammer-vlsi/hammer_vlsi/hammer_vlsi_impl.py
@@ -1476,7 +1476,7 @@ class HasSDCSupport(HammerTool):
                     groups[clock.group] = [clock.name]
             else:
                 ungrouped_clocks.append(clock.name)
-        if len(groups) + len(ungrouped_clocks):
+        if len(clocks):
             output.append("set_clock_groups -asynchronous {grouped} {ungrouped}".format(
                     grouped = " ".join(["-group {{ {c} }}".format(c=" ".join(clks)) for clks in groups.values()]),
                     ungrouped = " ".join(["-group {{ {c} }}".format(c=clk) for clk in ungrouped_clocks])
