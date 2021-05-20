050c5a06547b12507913c4aadc9f08d8ec5feedc
Dan Fritchman <dan@fritch.mn>
2021-05-20 12:56:13 -0700

    Single eFabless SRAM dual-port SRAM posing as single-port

--- src/hammer-vlsi/technology/sky130/sram_compiler/__init__.py
+++ src/hammer-vlsi/technology/sky130/sram_compiler/__init__.py
@@ -30,43 +30,46 @@ class SKY130SRAMGenerator(HammerSRAMGeneratorTool):
             speed = "TT"
 
         # Different target memories based on port count
+        # if params.family == "1rw":
+        #     self.logger.info("Compiling 1rw memories to DFFRAM instances")
+        #     base_dir = self.get_setting("technology.sky130.dffram_lib")
+        #     fam_code = params.family
+        #     sram_name = "RAM{d}x{w}".format(
+        #         d=params.depth,
+        #         w=params.width)
+        #     #TODO: need real libs (perhaps run Liberate here?)
+        #     #For now, use the dummy lib for all corners
+        #     corner_str = "" #
+        #     lib_path = "{b}/{n}.lib".format(
+        #         b=base_dir,
+        #         n=sram_name)
+        #     if not os.path.exists(lib_path):
+        #         self.logger.error("SKY130 1rw1r SRAM cache does not support corner: {c}".format(c=corner_str))
+        #     return ExtraLibrary(prefix=None, library=Library(
+        #         name=sram_name,
+        #         nldm_liberty_file=lib_path,
+        #         lef_file="{b}/{n}/{n}.lef".format(b=base_dir,n=sram_name),
+        #         #TODO: GDS not generated. Unclear which DEF to use?
+        #         #gds_file="{b}/{n}/{n}.gds".format(b=base_dir,n=sram_name),
+        #         spice_file="{b}/{n}/{n}.spice".format(b=base_dir,n=sram_name),
+        #         #TODO: Will not work as-is for behav. sim (this is a structural netlist referencing std. cells)
+        #         #Need to add std cell behavioral Verilog to sim.inputs.input_files
+        #         verilog_sim="{b}/{n}/{n}.nl.v".format(b=base_dir,n=sram_name),
+        #         corner={'nmos': speed_name, 'pmos': speed_name, 'temperature': str(corner.temp.value_in_units("C")) + " C"},
+        #         supplies={'VDD': str(corner.voltage.value_in_units("V")) + " V", 'GND': "0 V"},
+        #         provides=[{'lib_type': "sram", 'vt': params.vt}]))
+        # elif params.family == "1rw1r":
         if params.family == "1rw":
-            self.logger.info("Compiling 1rw memories to DFFRAM instances")
-            base_dir = self.get_setting("technology.sky130.dffram_lib")
-            fam_code = params.family
-            sram_name = "RAM{d}x{w}".format(
-                d=params.depth,
-                w=params.width)
-            #TODO: need real libs (perhaps run Liberate here?)
-            #For now, use the dummy lib for all corners
-            corner_str = "" #
-            lib_path = "{b}/{n}.lib".format(
-                b=base_dir,
-                n=sram_name)
-            if not os.path.exists(lib_path):
-                self.logger.error("SKY130 1rw1r SRAM cache does not support corner: {c}".format(c=corner_str))
-            return ExtraLibrary(prefix=None, library=Library(
-                name=sram_name,
-                nldm_liberty_file=lib_path,
-                lef_file="{b}/{n}/{n}.lef".format(b=base_dir,n=sram_name),
-                #TODO: GDS not generated. Unclear which DEF to use?
-                #gds_file="{b}/{n}/{n}.gds".format(b=base_dir,n=sram_name),
-                spice_file="{b}/{n}/{n}.spice".format(b=base_dir,n=sram_name),
-                #TODO: Will not work as-is for behav. sim (this is a structural netlist referencing std. cells)
-                #Need to add std cell behavioral Verilog to sim.inputs.input_files
-                verilog_sim="{b}/{n}/{n}.nl.v".format(b=base_dir,n=sram_name),
-                corner={'nmos': speed_name, 'pmos': speed_name, 'temperature': str(corner.temp.value_in_units("C")) + " C"},
-                supplies={'VDD': str(corner.voltage.value_in_units("V")) + " V", 'GND': "0 V"},
-                provides=[{'lib_type': "sram", 'vt': params.vt}]))
-        elif params.family == "1rw1r":
             self.logger.info("Compiling 1rw1r memories to OpenRAM instances")
             base_dir = self.get_setting("technology.sky130.openram_lib")
             fam_code = params.family
-            sram_name = "sky130_sram_{s}kbyte_1rw1r_{w}x{d}_{m}".format(
-                s=round(params.width*params.depth/8, -3)/1000, # size in kiB
-                w=params.width,
-                d=params.depth,
-                m=8) #TODO: Hammer SRAMParameters doesn't have this info
+            s=round(round(params.width*params.depth/8, -3)/1000) # size in kiB
+            w=params.width
+            d=params.depth
+            m=8
+            sram_name = f"sky130_sram_{s}kbyte_1rw1r_{w}x{d}_{m}"
+            print(f"SRAM_NAME: {sram_name}")
+            #TODO: Hammer SRAMParameters doesn't have this info
             #TODO: replace this if OpenRAM characterization done for other corners
             #For now, use typical lib for all corners
             corner_str = "TT_1p8V_25C"
