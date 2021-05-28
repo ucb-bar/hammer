8e27d78dd22617f50b5940f1a40c8f8113c4b32c
Dan Fritchman <dan@fritch.mn>
2021-05-27 17:06:02 -0700

    Copy Sky130 Layer-Map

--- src/hammer-vlsi/technology/sky130/__init__.py
+++ src/hammer-vlsi/technology/sky130/__init__.py
@@ -7,7 +7,7 @@
 
 import sys
 import re
-import os
+import os, shutil
 from pathlib import Path 
 #from typing import NamedTuple, List, Optional, Tuple, Dict, Set, Any
 
@@ -23,8 +23,12 @@ class SKY130Tech(HammerTechnology):
     def post_install_script(self) -> None:
         # maybe run open_pdks for the user and install in tech cache...?
         # this takes a while and ~7Gb
+        self.setup_techlef()
+        self.setup_layermap()
+        print('Loaded Sky130 Tech')
 
-        # hack tlef, adding this very important `licon` section 
+    def setup_techlef(self) -> None:
+        """ Copy and hack the tech-lef, adding this very important `licon` section """
         setting_dir = self.get_setting("technology.sky130.open_pdks")
         setting_dir = Path(setting_dir)
         library_name = 'sky130_fd_sc_hd'
@@ -32,7 +36,7 @@ class SKY130Tech(HammerTechnology):
         if not tlef_old_path.exists():
             raise FileNotFoundError(f"Tech-LEF not found: {tlef_old_path}")
 
-        cache_tech_dir_path = Path(self.cache_dir) / 'techlef'
+        cache_tech_dir_path = Path(self.cache_dir) 
         os.makedirs(cache_tech_dir_path, exist_ok=True)
         tlef_new_path = cache_tech_dir_path / f'{library_name}.tlef' 
 
@@ -42,10 +46,20 @@ class SKY130Tech(HammerTechnology):
             f_new.write(line)
             if line.strip() == 'END pwell':
                 f_new.write(_the_tlef_edit)
-
         f_old.close()
         f_new.close()
-        print('Loaded Sky130 Tech')
+
+    def setup_layermap(self) -> None:
+        """ Copy the layer-map into `self.cache_dir` """
+        nda_dir = self.get_setting("technology.sky130.sky130_nda")
+        nda_dir = Path(nda_dir)
+        layermap = nda_dir / "s8/V2.0.1/VirtuosoOA/libs/technology_library/technology_library.layermap"
+        if not layermap.exists():
+            raise FileNotFoundError(f"Layer-map not found: {layermap}")
+        cache_path = Path(self.cache_dir) 
+        os.makedirs(cache_path, exist_ok=True)
+        shutil.copy(layermap, cache_path)
+
         
 
 _the_tlef_edit = '''
