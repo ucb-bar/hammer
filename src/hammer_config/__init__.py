1d1c40e971be1b4ab16f4933c19b7cb2daf37164
Colin Schmidt <colin.schmidt@sifive.com>
2021-12-27 13:10:13 -0800

    Support relative directory meta application inside hierarchical constraints

--- src/hammer_config/__init__.py
+++ src/hammer_config/__init__.py
@@ -2,4 +2,5 @@
 
 # https://stackoverflow.com/questions/34461987/python3-importerror-no-module-named-xxxx
 from .config_src import *
+from .config_src import _CONFIG_PATH_KEY
 from .yaml2json import load_yaml
