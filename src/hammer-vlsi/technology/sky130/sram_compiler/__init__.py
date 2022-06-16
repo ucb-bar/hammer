fd4ee6a71762fcff0c4ecd1086b6d518c2db98d5
Nayiri K <nayiri@berkeley.edu>
2022-06-16 01:06:24 -0700

    sky130 plugin updates to support openroad + new PDK updates

--- src/hammer-vlsi/technology/sky130/sram_compiler/__init__.py
+++ src/hammer-vlsi/technology/sky130/sram_compiler/__init__.py
@@ -1,5 +1,6 @@
 
 import os, tempfile, subprocess
+from pathlib import Path
 
 from hammer_vlsi import MMMCCorner, MMMCCornerType, HammerTool, HammerToolStep, HammerSRAMGeneratorTool, SRAMParameters
 from hammer_vlsi.units import VoltageValue, TemperatureValue
@@ -16,7 +17,7 @@ class SKY130SRAMGenerator(HammerSRAMGeneratorTool):
 
     # Run generator for a single sram and corner
     def generate_sram(self, params: SRAMParameters, corner: MMMCCorner) -> ExtraLibrary:
-        tech_cache_dir = os.path.abspath(self.technology.cache_dir)
+        cache_dir = os.path.abspath(self.technology.cache_dir)
 
         #TODO: this is really an abuse of the corner stuff
         if corner.type == MMMCCornerType.Setup:
@@ -83,12 +84,15 @@ class SKY130SRAMGenerator(HammerSRAMGeneratorTool):
                 c=corner_str)
             if not os.path.exists(lib_path):
                 self.logger.error("SKY130 1rw1r SRAM cache does not support corner: {c}".format(c=corner_str))
+
+            self.setup_sram_spice(sram_name)
+            self.setup_sram_lef(sram_name)
             return ExtraLibrary(prefix=None, library=Library(
                 name=sram_name,
                 nldm_liberty_file=lib_path,
-                lef_file="{b}/{n}/{n}.lef".format(b=tech_cache_dir,n=sram_name),
+                lef_file="{b}/{n}/{n}.lef".format(b=cache_dir,n=sram_name),
                 gds_file="{b}/{n}/{n}.gds".format(b=base_dir,n=sram_name),
-                spice_file="{b}/{n}/{n}.lvs.sp".format(b=base_dir,n=sram_name),
+                spice_file="{b}/{n}/{n}.lvs.sp".format(b=cache_dir,n=sram_name),
                 verilog_sim="{b}/{n}/{n}.v".format(b=base_dir,n=sram_name),
                 corner={'nmos': speed_name, 'pmos': speed_name, 'temperature': str(corner.temp.value_in_units("C")) + " C"},
                 supplies={'VDD': str(corner.voltage.value_in_units("V")) + " V", 'GND': "0 V"},
@@ -96,5 +100,41 @@ class SKY130SRAMGenerator(HammerSRAMGeneratorTool):
         else:
             self.logger.error("SKY130 SRAM cache does not support family:{f}".format(f=params.family))
             return ExtraLibrary(prefix=None, library=None)
+
+    def setup_sram_spice(self,sram_name) -> None:
+        cache_dir = os.path.abspath(self.technology.cache_dir)
+        source_path = Path(self.get_setting("technology.sky130.openram_lib")) / sram_name / f"{sram_name}.lvs.sp"
+        dest_path = f"{os.path.abspath(self.technology.cache_dir)}/{sram_name}/{sram_name}.lvs.sp"
+        if not source_path.exists():
+            raise FileNotFoundError(f"SRAM Spice file not found: {source_path}")        
+        with open(source_path,'r') as sf:
+            with open(dest_path,'w') as df:
+                self.logger.info("Modifying SRAM SPICE deck: {} -> {}".format
+                    (source_path, dest_path))
+                for line in sf:
+                    line = line.replace('sky130_fd_pr__pfet_01v8','pshort')
+                    line = line.replace('sky130_fd_pr__nfet_01v8','nshort')
+                    if sram_name == "sky130_sram_1kbyte_1rw1r_8x1024_8":
+                        line = line.replace('wmask0[0]'    , 'wmask0')
+                    df.write(line)
+
+    def setup_sram_lef(self,sram_name) -> None:
+        source_path = Path(self.get_setting("technology.sky130.openram_lib")) / sram_name / f"{sram_name}.lef"
+        dest_path = f"{os.path.abspath(self.technology.cache_dir)}/{sram_name}/{sram_name}.lef"
+        if not source_path.exists():
+            raise FileNotFoundError(f"SRAM LEF file not found: {source_path}")
+        with open(source_path,'r') as sf:
+            with open(dest_path,'w') as df:
+                self.logger.info("Modifying SRAM LEF deck: {} -> {}".format
+                    (source_path, dest_path))
+                units=False
+                for line in sf:
+                    if line.strip().startswith("UNITS"):
+                        units=True
+                    if line.strip().startswith("END UNITS"):
+                        units=False
+                        continue
+                    if not units:
+                        df.write(line)
 
 tool=SKY130SRAMGenerator
