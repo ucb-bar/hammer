dc14969b861d93a5610231230fb2018aa77561d1
Nayiri K <nayiri@berkeley.edu>
2022-12-05 11:47:25 -0800

    changes for sky130 tapeout to include sram22

--- src/hammer-vlsi/technology/sky130/__init__.py
+++ src/hammer-vlsi/technology/sky130/__init__.py
@@ -8,6 +8,7 @@
 import sys
 import re
 import os, shutil
+import json
 from pathlib import Path
 from typing import NamedTuple, List, Optional, Tuple, Dict, Set, Any
 
@@ -115,10 +116,14 @@ class SKY130Tech(HammerTechnology):
             with open(dest_path, 'w') as df:
                 self.logger.info("Modifying Technology LEF: {} -> {}".format
                     (source_path, dest_path))
+                wrote_overlap = False
                 for line in sf:
+                    if not wrote_overlap and line.startswith('LAYER'):
+                        df.write(_tlef_overlap)
+                        wrote_overlap = True
                     df.write(line)
                     if line.strip() == 'END pwell':
-                        df.write(_the_tlef_edit)
+                        df.write(_tlef_licon)
 
     # Remove conflicting specification statements found in PDK LVS decks
     def setup_lvs_deck(self) -> None:
@@ -173,17 +178,20 @@ class SKY130Tech(HammerTechnology):
             ]}
         return hooks.get(tool_name, [])
 
-    @staticmethod
-    def openram_sram_names() -> List[str]:
-        """ Return a list of cell-names of the OpenRAM SRAMs (that we'll use). """
-        return [
-            "sky130_sram_1kbyte_1rw1r_32x256_8",
-            "sky130_sram_1kbyte_1rw1r_8x1024_8",
-            "sky130_sram_2kbyte_1rw1r_32x512_8"
-        ]
 
+sky130_sram_names = []
+with open(f"{Path(__file__).parent.resolve()}/sram-cache.json",'r') as f:
+    dl = json.load(f)
+    for d in dl:
+        sky130_sram_names.append(d['name'])
 
-_the_tlef_edit = '''
+_tlef_overlap = '''
+LAYER OVERLAP
+  TYPE OVERLAP ;
+END OVERLAP
+
+'''
+_tlef_licon = '''
 LAYER licon
   TYPE CUT ;
 END licon
@@ -204,7 +212,7 @@ LVS FILTER D  OPEN  LAYOUT
 '''
 
 # black-box SRAMs during LVS
-for name in SKY130Tech.openram_sram_names():
+for name in sky130_sram_names:
     LVS_DECK_INSERT_LINES += f"LVS BOX {name} \n"
     LVS_DECK_INSERT_LINES += f"LVS FILTER {name} OPEN \n"
 
@@ -222,21 +230,21 @@ def sky130_innovus_settings(ht: HammerTool) -> bool:
 #-------------------------------------------------------------------------------
 set_db place_global_place_io_pins  true
 
-set_db opt_honor_fences true
-set_db place_detail_dpt_flow true
-set_db place_detail_color_aware_legal true
-set_db place_global_solver_effort high
-set_db place_detail_check_cut_spacing true
-set_db place_global_cong_effort high
+# set_db opt_honor_fences true
+# set_db place_detail_dpt_flow true
+# set_db place_detail_color_aware_legal true
+# set_db place_global_solver_effort high
+# set_db place_detail_check_cut_spacing true
+# set_db place_global_cong_effort high
 
 ##########################################################
 # Optimization attributes  [get_db -category opt]
 ##########################################################
 #-------------------------------------------------------------------------------
 
-set_db opt_fix_fanout_load true
-set_db opt_clock_gate_aware false
-set_db opt_area_recovery true
+# set_db opt_fix_fanout_load true
+# set_db opt_clock_gate_aware false
+# set_db opt_area_recovery true
 set_db opt_post_route_area_reclaim setup_aware
 set_db opt_fix_hold_verbose true
 
@@ -247,8 +255,8 @@ set_db opt_fix_hold_verbose true
 set_db cts_target_skew 0.03
 set_db cts_max_fanout 10
 #set_db cts_target_max_transition_time .3
-set_db opt_setup_target_slack 0.10
-set_db opt_hold_target_slack 0.10
+# set_db opt_setup_target_slack 0.10
+# set_db opt_hold_target_slack 0.10
 
 ##########################################################
 # Routing attributes  [get_db -category route]
@@ -258,13 +266,15 @@ set_db route_design_antenna_diode_insertion 1
 set_db route_design_antenna_cell_name "sky130_fd_sc_hd__diode_2"
 set_db route_design_bottom_routing_layer 2
 
-set_db route_design_high_freq_search_repair true
-set_db route_design_detail_post_route_spread_wire true
-set_db route_design_with_si_driven true
-set_db route_design_with_timing_driven true
-set_db route_design_concurrent_minimize_via_count_effort high
-set_db opt_consider_routing_congestion true
-set_db route_design_detail_use_multi_cut_via_effort medium
+# set_db route_design_high_freq_search_repair true
+# set_db route_design_detail_post_route_spread_wire true
+# set_db route_design_with_si_driven true
+# set_db route_design_with_timing_driven true
+# set_db route_design_concurrent_minimize_via_count_effort high
+# set_db opt_consider_routing_congestion true
+# set_db route_design_detail_use_multi_cut_via_effort medium
+
+set_db extract_rc_effort_level low
     '''
     )
     return True
@@ -317,7 +327,7 @@ set_wire_rc -clock  -layer "met5"
 def drc_blackbox_openram_srams(ht: HammerTool) -> bool:
     assert isinstance(ht, HammerDRCTool), "Exlude SRAMs only in DRC"
     drc_box = ''
-    for name in SKY130Tech.openram_sram_names():
+    for name in sky130_sram_names:
         drc_box += f"\nEXCLUDE CELL {name}"
     run_file = ht.drc_run_file  # type: ignore
     with open(run_file, "a") as f:
@@ -327,7 +337,7 @@ def drc_blackbox_openram_srams(ht: HammerTool) -> bool:
 def lvs_blackbox_openram_srams(ht: HammerTool) -> bool:
     assert isinstance(ht, HammerLVSTool), "Blackbox and filter SRAMs only in LVS"
     lvs_box = ''
-    for name in SKY130Tech.openram_sram_names():
+    for name in sky130_sram_names:
         lvs_box += f"\nLVS BOX {name}"
         lvs_box += f"\nLVS FILTER {name} OPEN "
     run_file = ht.lvs_run_file  # type: ignore
