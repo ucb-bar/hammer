a6d76c8e9bfd961876b0f3260042b703caf6d2ba
Harrison Liew <harrisonliew@gmail.com>
2022-12-19 16:15:25 -0800

    restore prependlocal/deepsubst_local tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -361,6 +361,42 @@ my:
             db.update_core([base, meta], self.NO_TYPES)
             db.get_setting("bad_list", check_type=False)
 
+    def test_meta_prependlocal(self) -> None:
+        """
+        Test that the meta attribute "prependlocal" works.
+        """
+        db = hammer_config.HammerDatabase()
+        base = hammer_config.load_config_from_string("""
+foo:
+    bar:
+        adc: "yes"
+        dac: "no"
+        dsl: ["scala"]
+        base_test: "local_path"
+        base_test_meta: prependlocal
+""", is_yaml=True, path="base/config/path")
+        meta = hammer_config.load_config_from_string("""
+{
+  "foo.bar.dsl": ["python"],
+  "foo.bar.dsl_meta": "append",
+  "foo.bar.dac": "current_weighted",
+  "foo.bar.meta_test": "local_path",
+  "foo.bar.meta_test_meta": "prependlocal"
+}
+""", is_yaml=False, path="meta/config/path")
+        array = hammer_config.load_config_from_string("""
+foo:
+    bar:
+        array_test: ["local_path", "express_path"]
+        array_test_meta: prependlocal
+""", is_yaml=True, path="array/config/path")
+        db.update_core([base, meta, array], self.NO_TYPES)
+        assert db.get_setting("foo.bar.dac", check_type=False) == "current_weighted"
+        assert db.get_setting("foo.bar.dsl", check_type=False) == ["scala", "python"]
+        assert db.get_setting("foo.bar.base_test", check_type=False) == "base/config/path/local_path"
+        assert db.get_setting("foo.bar.meta_test", check_type=False) == "meta/config/path/local_path"
+        assert db.get_setting("foo.bar.array_test", check_type=False) == ["array/config/path/local_path", "array/config/path/express_path"]
+
     def test_meta_transclude(self) -> None:
         """
         Test that the meta attribute "transclude" works.
@@ -742,6 +778,41 @@ foo:
         db.update_core([base, meta], self.NO_TYPES)
         assert db.get_setting("foo.bar", check_type=False)[0]["base_test"] == os.path.join(cwd, "some_relative_path")
         assert db.get_setting("foo.bar", check_type=False)[1]["meta_test"] == os.path.join(cwd, "some/relative/path")
+        # leading / should override the meta
+        assert db.get_setting("foo.bar", check_type=False)[1]["abs_test"] == "/this/is/an/abs/path"
+
+    def test_meta_deepsubst_local(self) -> None:
+        """
+        Test that the deepsubst special meta "local" correctly prepends the local path.
+        """
+        db = hammer_config.HammerDatabase()
+        base = hammer_config.load_config_from_string("""
+foo:
+    bar_meta: deepsubst
+    bar:
+    - adc: "yes"
+      dac: "no"
+      dsl: ["scala"]
+      base_test: "local_path"
+      base_test_deepsubst_meta: "local"
+""", is_yaml=True, path="base/config/path")
+        meta = hammer_config.load_config_from_string("""
+{
+  "foo.bar_meta": ["append", "deepsubst"],
+  "foo.bar": [{
+      "dsl": ["python"],
+      "dsl_meta": "append",
+      "dac": "current_weighted",
+      "meta_test": "local_path",
+      "meta_test_deepsubst_meta": "local",
+      "abs_test": "/this/is/an/abs/path",
+      "abs_test_deepsubst_meta": "local"
+  }]
+}
+""", is_yaml=False, path="meta/config/path")
+        db.update_core([base, meta], self.NO_TYPES)
+        assert db.get_setting("foo.bar", check_type=False)[0]["base_test"] == "base/config/path/local_path"
+        assert db.get_setting("foo.bar", check_type=False)[1]["meta_test"] == "meta/config/path/local_path"
         # leading / should override the meta
         assert db.get_setting("foo.bar", check_type=False)[1]["abs_test"] == "/this/is/an/abs/path"
 
