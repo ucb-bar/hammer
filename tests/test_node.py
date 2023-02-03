4a41864f741c59839b0a35cc1ca948c19142c5df
Bryan Ngo <bryanngo@berkeley.edu>
2023-02-03 11:50:20 -0800

    more cycle reduction tests

--- tests/test_node.py
+++ tests/test_node.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 import unittest
+from textwrap import dedent
 
 import networkx as nx
 import pytest
@@ -16,7 +17,7 @@ class NodeTest(unittest.TestCase):
         """Test that we can instantiate a node."""
         test = Node(
             "syn", "nop", "syn_dir", "",
-            ["bryan-test.yml"],
+            ["foo.yml"],
             ["syn-out.json"],
         )
         self.assertEqual(test.action, "syn")
@@ -25,7 +26,7 @@ class NodeTest(unittest.TestCase):
         """Creating a more complex graph."""
         root = Node(
             "syn", "nop", "syn_dir", "",
-            ["bryan-test.yml"],
+            ["foo.yml"],
             ["syn-out.json"],
         )
         child1 = Node(
@@ -34,9 +35,24 @@ class NodeTest(unittest.TestCase):
             ["par-out.json"],
         )
         graph = Graph({root: [child1]})
-        self.assertTrue(graph.verify())
         self.assertEqual(len(graph.networkx), 2)
         self.assertEqual(graph.networkx.number_of_edges(), 1)
+        self.assertTrue(graph.verify())
+
+    def test_invalid_graph(self) -> None:
+        """Test that invalid flowgraphs are detected."""
+        root = Node(
+            "syn", "nop", "syn_dir", "",
+            ["foo.yml"],
+            ["syn-out.json"],
+        )
+        child1 = Node(
+            "par", "nop", "syn_dir", "par_dir",
+            ["syn-in.json"],
+            ["par-out.json"],
+        )
+        graph = Graph({root: [child1]})
+        self.assertFalse(graph.verify())
 
     def test_cycle_reduction(self) -> None:
         """Test that cycles are reduced to an acyclic graph."""
@@ -66,6 +82,86 @@ class NodeTest(unittest.TestCase):
             nx.find_cycle(g_acyclic.networkx)
         self.assertEqual(len(g_acyclic.networkx), 4)
         self.assertEqual(g.networkx.number_of_edges(), 3)
+
+    def test_big_cycle_reduction(self) -> None:
+        """Test that larger cycles are reduced to an acyclic graph."""
+        v0 = Node(
+            "v0", "nop", "v0_dir", "v1_dir",
+            ["v0-in.json"],
+            ["v0-out.json"],
+        )
+        v1 = Node(
+            "v1", "nop", "v1_dir", "v2_dir",
+            ["v0-out.json"],
+            ["v1-out.json"],
+        )
+        v2 = Node(
+            "v2", "nop", "v2_dir", "v3_dir",
+            ["v1-out.json"],
+            ["v2-out.json"],
+        )
+        v3 = Node(
+            "v3", "nop", "v3_dir", "v0_dir",
+            ["v2-out.json"],
+            ["v3-out.json"],
+        )
+        g = Graph({
+            v0: [v1],
+            v1: [v2],
+            v2: [v3],
+            v3: [v0]
+        })
+        self.assertTrue(len(nx.find_cycle(g.networkx)) > 0)
+        g_acyclic = node.convert_to_acyclic(g)
+        with self.assertRaises(nx.NetworkXNoCycle):
+            nx.find_cycle(g_acyclic.networkx)
+        self.assertEqual(len(g_acyclic.networkx), 5)
+        self.assertEqual(g.networkx.number_of_edges(), 4)
+
+    def test_multi_cycle_reduction(self) -> None:
+        """Test that multiple cycles are reduced to an acyclic graph."""
+        v0 = Node(
+            "v0", "nop", "v0_dir", "v1_dir",
+            ["v0-in.json"],
+            ["v0-out.json"],
+        )
+        v1 = Node(
+            "v1", "nop", "v1_dir", "v2_dir",
+            ["v0-out.json"],
+            ["v1-out.json"],
+        )
+        v2 = Node(
+            "v2", "nop", "v2_dir", "v0_dir",
+            ["v1-out.json"],
+            ["v2-out.json"],
+        )
+        v3 = Node(
+            "v3", "nop", "v3_dir", "v4_dir",
+            ["v2-out.json"],
+            ["v3-out.json"],
+        )
+        v4 = Node(
+            "v4", "nop", "v4_dir", "v5_dir",
+            ["v3-out.json"],
+            ["v4-out.json"],
+        )
+        v5 = Node(
+            "v5", "nop", "v5_dir", "v3_dir",
+            ["v4-out.json"],
+            ["v3-out.json"],
+        )
+        g = Graph({
+            v0: [v1],
+            v1: [v2],
+            v2: [v0, v3],
+            v3: [v4, v2],
+            v4: [v5],
+            v5: [v3]
+        })
+        self.assertTrue(len(nx.find_cycle(g.networkx)) > 0)
+        g_acyclic = node.convert_to_acyclic(g)
+        with self.assertRaises(nx.NetworkXNoCycle):
+            nx.find_cycle(g_acyclic.networkx)
 
     @pytest.mark.xfail(raises=ImportError, reason="Matplotlib currently not imported")
     def test_visualization(self) -> None:
@@ -130,53 +226,54 @@ class NodeTest(unittest.TestCase):
         HammerVLSILogging.clear_callbacks()
         HammerVLSILogging.add_callback(HammerVLSILogging.callback_buffering)
 
+        cfg = dedent("""
+            synthesis.inputs:
+                input_files: ["LICENSE", "README.md"]
+                top_module: "z1top.xdc"
+
+            par.inputs:
+                input_files: ["LICENSE", "README.md"]
+                top_module: "z1top.xdc"
+
+            drc.inputs:
+                input_files: ["LICENSE", "README.md"]
+                top_module: "z1top.xdc"
+
+            lvs.inputs:
+                input_files: ["LICENSE", "README.md"]
+                schematic_files: ["LICENSE", "README.md"]
+                top_module: "z1top.xdc"
+                hcells_list: []
+
+            pcb.inputs:
+                top_module: "z1top.xdc"
+
+            formal.inputs:
+                input_files: ["LICENSE", "README.md"]
+                top_module: "z1top.xdc"
+
+            sim.inputs:
+                input_files: ["LICENSE", "README.md"]
+                top_module: "z1top.xdc"
+
+            vlsi:
+                core:
+                    technology: "hammer.technology.nop"
+
+                    synthesis_tool: "hammer.synthesis.nop"
+                    par_tool: "hammer.par.nop"
+                    drc_tool: "hammer.drc.nop"
+                    lvs_tool: "hammer.lvs.nop"
+                    power_tool: "hammer.power.nop"
+                    sim_tool: "mocksim"
+        """)
+
         with tempfile.TemporaryDirectory() as td:
             os.mkdir(os.path.join(td, "syn_dir"))
             os.mkdir(os.path.join(td, "par_dir"))
-            os.mkdir(os.path.join(td, "drc_dir"))
 
             with open(os.path.join(td, "syn_dir", "syn-in.yml"), 'w', encoding="utf-8") as tf1:
-                tf1.write("""
-synthesis.inputs:
-    input_files: ["LICENSE", "README.md"]
-    top_module: "z1top.xdc"
-
-par.inputs:
-    input_files: ["LICENSE", "README.md"]
-    top_module: "z1top.xdc"
-
-drc.inputs:
-    input_files: ["LICENSE", "README.md"]
-    top_module: "z1top.xdc"
-
-lvs.inputs:
-    input_files: ["LICENSE", "README.md"]
-    schematic_files: ["LICENSE", "README.md"]
-    top_module: "z1top.xdc"
-    hcells_list: []
-
-pcb.inputs:
-    top_module: "z1top.xdc"
-
-formal.inputs:
-    input_files: ["LICENSE", "README.md"]
-    top_module: "z1top.xdc"
-
-sim.inputs:
-    input_files: ["LICENSE", "README.md"]
-    top_module: "z1top.xdc"
-
-vlsi:
-    core:
-        technology: "hammer.technology.nop"
-
-        synthesis_tool: "hammer.synthesis.nop"
-        par_tool: "hammer.par.nop"
-        drc_tool: "hammer.drc.nop"
-        lvs_tool: "hammer.lvs.nop"
-        power_tool: "hammer.power.nop"
-        sim_tool: "mocksim"
-""")
+                tf1.write(cfg)
 
             syn = Node(
                 "syn", "nop",
@@ -205,6 +302,96 @@ vlsi:
 
         for n in g.networkx:
             self.assertEqual(n.status, Status.COMPLETE)
+
+    def test_invalid_run(self) -> None:
+        """
+        Test that a failing run should not run
+        other steps and exit gracefully.
+        """
+        HammerVLSILogging.clear_callbacks()
+        HammerVLSILogging.add_callback(HammerVLSILogging.callback_buffering)
+
+        cfg = dedent("""
+            synthesis.inputs:
+                input_files: ["LICENSE", "README.md"]
+                top_module: "z1top.xdc"
+
+            par.inputs:
+                input_files: ["LICENSE", "README.md"]
+                top_module: "z1top.xdc"
+
+            drc.inputs:
+                input_files: ["LICENSE", "README.md"]
+                top_module: "z1top.xdc"
+
+            lvs.inputs:
+                input_files: ["LICENSE", "README.md"]
+                schematic_files: ["LICENSE", "README.md"]
+                top_module: "z1top.xdc"
+                hcells_list: []
+
+            pcb.inputs:
+                top_module: "z1top.xdc"
+
+            formal.inputs:
+                input_files: ["LICENSE", "README.md"]
+                top_module: "z1top.xdc"
+
+            sim.inputs:
+                input_files: ["LICENSE", "README.md"]
+                top_module: "z1top.xdc"
+
+            vlsi:
+                core:
+                    technology: "hammer.technology.nop"
+
+                    synthesis_tool: "hammer.synthesis.nop"
+                    par_tool: "hammer.par.nop"
+                    drc_tool: "hammer.drc.nop"
+                    lvs_tool: "hammer.lvs.nop"
+                    power_tool: "hammer.power.nop"
+                    sim_tool: "mocksim"
+        """)
+
+        with tempfile.TemporaryDirectory() as td:
+            os.mkdir(os.path.join(td, "syn_dir"))
+            os.mkdir(os.path.join(td, "par_dir"))
+
+            with open(os.path.join(td, "syn_dir", "syn-in.yml"), 'w', encoding="utf-8") as tf1:
+                tf1.write(cfg)
+
+            syn = Node(
+                "syn", "nop",
+                os.path.join(td, "syn_dir"), os.path.join(td, "s2p_dir"),
+                ["syn-in.yml"],
+                ["syn-out.json"],
+            )
+            s2p_bad = Node(
+                "blah", "nop",
+                os.path.join(td, "s2p_dir"), os.path.join(td, "par_dir"),
+                ["syn-out.json"],
+                ["s2p-out.json"],
+            )
+            par = Node(
+                "par", "nop",
+                os.path.join(td, "par_dir"), os.path.join(td, "out_dir"),
+                ["s2p-out.json"],
+                ["par-out.json"],
+            )
+            g = Graph({
+                syn: [s2p_bad],
+                s2p_bad: [par],
+                par: []
+            })
+            with pytest.raises(Exception):
+                g.run(syn)
+
+            print(syn.status)
+            print(s2p_bad.status)
+            print(par.status)
+            self.assertEqual(syn.status, Status.COMPLETE)
+            self.assertEqual(s2p_bad.status, Status.INCOMPLETE)
+            self.assertEqual(par.status, Status.NOT_RUN)
 
 
 if __name__ == "__main__":
