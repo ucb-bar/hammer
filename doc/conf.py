812b763950f71ef88a2af5f5c3aac6a55f26b8b5
Bryan Ngo <bryanngo@berkeley.edu>
2023-03-27 21:33:32 -0700

    add mermaid support to docs

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,8 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_parser"
+    "myst_parser",
+    "sphinxcontrib.mermaid"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
